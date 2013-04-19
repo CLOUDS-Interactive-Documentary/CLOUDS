7d785f46dd037f8c5696b4785de8d55779b6b418
James George <james@jamesgeorge.org>
2013-04-19 00:25:48 -0400

    experiments to visualize entire congo network

---
+++ ConversationNetwork/src/testApp.cpp
@@ -0,0 +1,63 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+
+    parser.setup("../../../CloudsLibrary/data/fcpxml");
+	conversation.setup(parser);
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+	conversation.update();
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	conversation.draw();
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
