4eed43ff6a5770a68977e0b5e2f5bfb1b2bcdbd4
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2013-05-25 15:46:12 -0700

    LSystem CloudsVisualsystem

--- VSLSys/src/CloudsVisualSystemLSys.cpp
+++ VSLSys/src/CloudsVisualSystemLSys.cpp
@@ -7,3 +7,156 @@
 //
 
 #include "CloudsVisualSystemLSys.h"
+
+CloudsVisualSystemLSys::CloudsVisualSystemLSys(){
+    
+	int buttonWidth = 380;
+	int buttonHeight = 30;
+    gui = new ofxUICanvas(0,0,buttonWidth,ofGetHeight());    
+    gui->addLabel("LSystem");
+    gui->addTextInput("Axiom", "B");
+    gui->addTextInput("Rule1", "B=F[5+B][7-B]-F[4+B][6-B]-[3+B][5+B]-FB");
+    gui->addTextInput("Rule2", "F=FF");
+    gui->addSlider("Angle", 0, 90, 5);
+    gui->addSlider("Scale", 0.5, 2, 1.5);
+    gui->addSlider("Depth", 1, 8, 4);
+    
+    gui->addLabel("Growing");
+    gui->addSlider("Speed", 0.0, 10, 1.0);
+    
+    gui->disable();
+    gui->loadSettings("GUI/guiSettings.xml");
+    
+    ofAddListener(gui->newGUIEvent,this,&CloudsVisualSystemLSys::guiEvent);
+    
+    ofxUISlider *uiAngle = (ofxUISlider *) gui->getWidget("Angle");
+    angle = uiAngle->getScaledValue();
+    ofxUITextInput *uiAxiom = (ofxUITextInput *) gui->getWidget("Axiom");
+    axiom = uiAxiom->getTextString();
+    ofxUITextInput *uiRule1 = (ofxUITextInput *) gui->getWidget("Rule1");
+    rule1 = uiRule1->getTextString();
+    ofxUITextInput *uiRule2 = (ofxUITextInput *) gui->getWidget("Rule2");
+    rule2 = uiRule2->getTextString();
+    ofxUISlider *uiScale = (ofxUISlider *) gui->getWidget("Scale");
+    lsysScale = uiScale->getScaledValue();
+    ofxUISlider *uiDepth = (ofxUISlider *) gui->getWidget("Depth");
+    lsysDepth = uiDepth->getScaledValue();
+    ofxUISlider *uiSpeed = (ofxUISlider *) gui->getWidget("Speed");
+    lsysr.speed = uiSpeed->getScaledValue();
+
+    reBuildLSys();
+}
+
+CloudsVisualSystemLSys::~CloudsVisualSystemLSys(){
+    
+}
+
+string CloudsVisualSystemLSys::getSystemName(){
+    return "LSystem";
+}
+
+void CloudsVisualSystemLSys::begin(){
+    gui->enable();
+}
+
+void CloudsVisualSystemLSys::end(){
+    gui->disable();
+}
+
+void CloudsVisualSystemLSys::guiEvent(ofxUIEventArgs &e){
+    string name = e.widget->getName();
+	int kind = e.widget->getKind();
+    
+    if(name == "Angle"){
+        ofxUISlider *uiAngle = (ofxUISlider *) gui->getWidget("Angle");
+        angle = uiAngle->getScaledValue();
+        reBuildLSys();
+    } else if ( name == "Axiom"){
+        ofxUITextInput *uiAxiom = (ofxUITextInput *) gui->getWidget("Axiom");
+        axiom = uiAxiom->getTextString();
+        reBuildLSys();
+    } else if ( name == "Rule1"){
+        ofxUITextInput *uiRule1 = (ofxUITextInput *) gui->getWidget("Rule1");
+        rule1 = uiRule1->getTextString();
+        reBuildLSys();
+    } else if ( name == "Rule2"){
+        ofxUITextInput *uiRule2 = (ofxUITextInput *) gui->getWidget("Rule2");
+        rule2 = uiRule2->getTextString();
+        reBuildLSys();
+    } else if ( name == "Scale"){
+        ofxUISlider *uiScale = (ofxUISlider *) gui->getWidget("Scale");
+        lsysScale = uiScale->getScaledValue();
+        reBuildLSys();
+    } else if ( name == "Depth"){
+        ofxUISlider *uiDepth = (ofxUISlider *) gui->getWidget("Depth");
+        lsysDepth = uiDepth->getScaledValue();
+        reBuildLSys();
+    } else if ( name == "Speed"){
+        ofxUISlider *uiSpeed = (ofxUISlider *) gui->getWidget("Speed");
+        lsysr.speed = uiSpeed->getScaledValue();
+        reBuildLSys();
+    }
+}
+
+void CloudsVisualSystemLSys::reBuildLSys(){
+    LSystem sys;
+    sys.initialPos.set(0,0);
+    
+    sys.ds = lsysScale;
+    
+    sys.setAngle(angle);
+    sys.addAxion(axiom);
+    sys.addRule(rule1);
+    if (rule2.size() > 0){
+        sys.addRule(rule2);
+    }
+    
+    lsysr.setup(sys, lsysDepth);
+    lsysr.start();
+}
+
+void CloudsVisualSystemLSys::update(ofEventArgs & args){
+    lsysr.update();
+    
+    ofSetWindowTitle(ofToString(ofGetFrameRate()));
+}
+
+void CloudsVisualSystemLSys::draw(ofEventArgs & args){
+    ofBackground(0);
+    
+    ofPushMatrix();
+    ofTranslate(ofGetWidth()*0.5, ofGetHeight());
+    ofScale(2,2);
+    ofSetColor(255);
+    lsysr.draw();
+    ofPopMatrix();
+}
+
+void CloudsVisualSystemLSys::exit(ofEventArgs & args){
+    gui->saveSettings("GUI/guiSettings.xml");
+    delete gui;
+}
+
+void CloudsVisualSystemLSys::keyPressed(ofKeyEventArgs & args){
+    
+}
+
+void CloudsVisualSystemLSys::keyReleased(ofKeyEventArgs & args){
+    
+}
+
+void CloudsVisualSystemLSys::mouseDragged(ofMouseEventArgs & args){
+    
+}
+
+void CloudsVisualSystemLSys::mouseMoved(ofMouseEventArgs & args){
+    
+}
+
+void CloudsVisualSystemLSys::mousePressed(ofMouseEventArgs & args){
+    
+}
+
+void CloudsVisualSystemLSys::mouseReleased(ofMouseEventArgs & args){
+    
+}
--- VSLSys/src/CloudsVisualSystemLSys.h
+++ VSLSys/src/CloudsVisualSystemLSys.h
@@ -6,9 +6,49 @@
 //
 //
 
-#ifndef __VSLSys__CloudsVisualSystemLSys__
-#define __VSLSys__CloudsVisualSystemLSys__
+#pragma once
 
-#include <iostream>
+#include "CloudsVisualSystem.h"
 
-#endif /* defined(__VSLSys__CloudsVisualSystemLSys__) */
+#include "ofxUI.h"
+#include "LSystemReconstructor.h"
+
+class CloudsVisualSystemLSys : public CloudsVisualSystem {
+public:
+	CloudsVisualSystemLSys();
+	~CloudsVisualSystemLSys();
+    
+    void update(ofEventArgs & args);
+    void draw(ofEventArgs & args);
+	
+    void exit(ofEventArgs & args);
+    
+	void keyPressed(ofKeyEventArgs & args);
+	void keyReleased(ofKeyEventArgs & args);
+	
+	void mouseDragged(ofMouseEventArgs & args);
+	void mouseMoved(ofMouseEventArgs & args);
+	void mousePressed(ofMouseEventArgs & args);
+	void mouseReleased(ofMouseEventArgs & args);
+    
+    string getSystemName();
+    
+protected:
+    void begin();
+	void end();
+    void reBuildLSys();
+    
+    LSystemReconstructor lsysr;
+    
+    string  axiom;
+    string  rule1;
+    string  rule2;
+    float   angle;
+    float   globalScale;
+    float   lsysScale;
+    int     lsysDepth;
+    
+    ofxUICanvas *gui;
+	void guiEvent(ofxUIEventArgs &e);
+
+};
