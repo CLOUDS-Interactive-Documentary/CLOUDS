c46aba4a56a1082ed869c1334741b79f77815dad
James George <james@jamesgeorge.org>
2013-05-26 13:35:34 -0700

    intro sequence test project

---
+++ VSIntroSequence/src/testApp.cpp
@@ -0,0 +1,73 @@
+#include "testApp.h"
+#include "ofxTimeline.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+	
+	ofxTimeline::removeCocoaMenusFromGlut("CLOUDS");
+	ofSetVerticalSync(true);
+	
+	intro.setup();
+	intro.playSystem();
+}
+
+
+//--------------------------------------------------------------
+void testApp::update(){
+
+}
+
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::exit(){
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
