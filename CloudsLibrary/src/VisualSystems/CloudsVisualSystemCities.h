c3725bf345a605b31df45c8f6b61faff9d7e6c07
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2013-05-26 19:23:34 -0700

    Cities + chroma AB

---
+++ CloudsLibrary/src/VisualSystems/CloudsVisualSystemCities.h
@@ -0,0 +1,66 @@
+//
+//  CloudsVisualSystemCities.h
+//  VSCloudsRezanator
+//
+//  Created by Patricio Gonzalez Vivo on 5/26/13.
+//
+//
+
+#pragma once
+
+#include "CloudsVisualSystemRezanator.h"
+
+class CloudsVisualSystemCities : public CloudsVisualSystemRezanator {
+public:
+    
+    string getSystemName();
+    
+    void selfSetup();
+    void selfSetupGuis();
+    
+    void selfAutoMode();
+    void selfUpdate();
+    void selfDrawBackground();
+    void selfDrawDebug();
+    void selfSceneTransformation();
+    void selfDraw();
+    void selfExit();
+    void selfBegin();
+	void selfEnd();
+    
+    void selfKeyPressed(ofKeyEventArgs & args);
+    void selfKeyReleased(ofKeyEventArgs & args);
+    
+    void selfMouseDragged(ofMouseEventArgs& data);
+    void selfMouseMoved(ofMouseEventArgs& data);
+    void selfMousePressed(ofMouseEventArgs& data);
+    void selfMouseReleased(ofMouseEventArgs& data);
+    
+    void selfSetupGui();
+    void selfGuiEvent(ofxUIEventArgs &e);
+    
+    void selfSetupSystemGui();
+    void guiSystemEvent(ofxUIEventArgs &e);
+    
+    void selfSetupRenderGui();
+    void guiRenderEvent(ofxUIEventArgs &e);
+    
+    void billBoard();
+    
+protected:
+    void    makeGrid(float _size, int _nGrains);
+    
+    ofShader noiseShader;
+    ofFbo    noiseFbo;
+    
+    ofShader maskShader;
+    ofFbo   maskFbo;
+    
+    GLuint  cube;
+    
+    float   size, nGrains, grainResolution;
+    float   noiseSpeed,noiseZoom;
+    float   maskSize,maskCurve;
+    float   minSize,maxHeight,minAlpha,minDist;
+    
+};
