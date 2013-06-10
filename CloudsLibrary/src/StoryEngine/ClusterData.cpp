818fc3208cd2891d793735d5bb88b5dde2c379e0
Surya Mattu <surya@suryamattu.com>
2013-06-10 11:31:01 -0400

    Added ClusterData into clouds clip

---
+++ CloudsLibrary/src/StoryEngine/ClusterData.cpp
@@ -0,0 +1,9 @@
+//
+//  ClusterData.cpp
+//  CloudsSVGTest
+//
+//  Created by Surya Mattu on 09/06/13.
+//
+//
+
+#include "ClusterData.h"
---
+++ CloudsLibrary/src/StoryEngine/ClusterData.h
@@ -0,0 +1,14 @@
+//
+//  ClusterData.h
+//  CloudsSVGTest
+//
+//  Created by Surya Mattu on 09/06/13.
+//
+//
+
+#ifndef __CloudsSVGTest__ClusterData__
+#define __CloudsSVGTest__ClusterData__
+
+#include <iostream>
+
+#endif /* defined(__CloudsSVGTest__ClusterData__) */
