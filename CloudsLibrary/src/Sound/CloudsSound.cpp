e3b974d421e862fbf8823ae907b528353d700891
James George <james@jamesgeorge.org>
2013-06-13 18:19:45 -0400

    moved sound libs to shared directory

---
+++ CloudsLibrary/src/Sound/CloudsSound.cpp
@@ -0,0 +1,9 @@
+//
+//  CloudsSound.cpp
+//  CLOUDS
+//
+//  Created by James George on 6/13/13.
+//
+//
+
+#include "CloudsSound.h"
---
+++ CloudsLibrary/src/Sound/CloudsSound.h
@@ -0,0 +1,14 @@
+//
+//  CloudsSound.h
+//  CLOUDS
+//
+//  Created by James George on 6/13/13.
+//
+//
+
+#ifndef __CLOUDS__CloudsSound__
+#define __CLOUDS__CloudsSound__
+
+#include <iostream>
+
+#endif /* defined(__CLOUDS__CloudsSound__) */
