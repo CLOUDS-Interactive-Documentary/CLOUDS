c9843e56dce17a303591095827f235e75ada25cc
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2013-06-13 13:34:58 -0400

    PaintBrush adaptation finish

--- CloudsLibrary/src/VisualSystems/CloudsVisualSystemPaintBrush.cpp
+++ CloudsLibrary/src/VisualSystems/CloudsVisualSystemPaintBrush.cpp
@@ -7,3 +7,240 @@
 //
 
 #include "CloudsVisualSystemPaintBrush.h"
+
+string CloudsVisualSystemPaintBrush::getSystemName()
+{
+	return "PaintBrush";
+}
+
+void CloudsVisualSystemPaintBrush::selfSetup()
+{
+    iconSize = 100;
+    
+    canvas.allocate(ofGetScreenWidth(), ofGetScreenHeight());
+    canvas.begin();
+    ofClear(0,0);
+    canvas.end();
+    
+    palette.path = getDataPath()+"visualsystems/PaintBrush/";
+    palette.setup();
+    palette.loadPalette(getDataPath()+"visualsystems/PaintBrush/settings.xml");
+    palette.setVisible(false);
+    
+    paletteBtn.set(0, 0, iconSize+30, iconSize+30);
+    paletteBtn.setImage(getDataPath()+"visualsystems/PaintBrush/icon_pallete.png");
+    ofAddListener( paletteBtn.clickPressed, this, &CloudsVisualSystemPaintBrush::showPalette );
+    cleanBtn.set(ofGetWidth()-iconSize,ofGetHeight()-iconSize,iconSize,iconSize);
+    ofAddListener( cleanBtn.clickPressed, this, &CloudsVisualSystemPaintBrush::cleanCanvas );
+    cleanBtn.setImage(getDataPath()+"visualsystems/PaintBrush/icon_close.png");
+    
+    bDebug  = false;
+}
+
+void CloudsVisualSystemPaintBrush::selfSetupSystemGui()
+{
+    sysGui->addSlider("brush_width",0, 50, &brushWidth);
+    sysGui->addSlider("number_of_lines", 1, 50, &brushNumber);
+    sysGui->addSlider("line_width", 1, 10, &lineWidth);
+    sysGui->addSlider("damp", 0, 0.5, &brushDamp);
+    sysGui->addSlider("k", 0.0, 0.5, &brushK);
+    
+    sysGui->addSlider("repulsion_rad", 0, 20, &brushRepRad);
+    sysGui->addSlider("repulsion_pct", 0.0, 1.0, &brushRepPct);
+    
+    sysGui->addSlider("color_lerp",0.0, 1.0, &colorLerp);
+    sysGui->addSlider("color_random", 0.0, 0.02, &colorRandom);
+}
+
+void CloudsVisualSystemPaintBrush::selfSetupRenderGui()
+{
+    
+}
+
+void CloudsVisualSystemPaintBrush::guiSystemEvent(ofxUIEventArgs &e)
+{
+    
+}
+
+void CloudsVisualSystemPaintBrush::selfKeyPressed(ofKeyEventArgs & args){
+    
+}
+
+void CloudsVisualSystemPaintBrush::selfUpdate()
+{
+    brush.update();
+    palette.update();
+    
+    canvas.begin();
+    if ( palette.getVisible() ){
+        if ( ofGetMouseY() > palette.getY() ){
+            brush.draw();
+        }
+    } else {
+        brush.draw();
+    }
+    canvas.end();
+    
+    if ( palette.getVisible() ){
+        if ( ofGetMouseY() < palette.getY() ){
+            palette.begin();
+            brush.draw();
+            palette.end();
+        }
+    }
+}
+
+void CloudsVisualSystemPaintBrush::draw(ofEventArgs & args)
+{
+    ofPushStyle();
+    if(bRenderSystem)
+    {
+        //  TEMPORAL FIX
+        //
+        drawBackground();
+        
+        canvas.draw(0, 0);
+        
+        paletteBtn.draw();
+        cleanBtn.draw();
+        
+        palette.draw();
+        
+        if (bDebug){
+            ofSetColor(255);
+            brush.drawDebug();
+        }
+        
+    }
+    
+    ofPopStyle();
+	
+    cleanBtn.set(ofGetWidth()-iconSize,ofGetHeight()-iconSize,iconSize,iconSize);
+    
+//	timeline->draw();
+}
+
+void CloudsVisualSystemPaintBrush::showPalette(int &_n){
+    if (!palette.getVisible()){
+        palette.clear();
+        brush.clear();
+        palette.setVisible(true);
+    }
+}
+
+void CloudsVisualSystemPaintBrush::cleanCanvas(int &_n){
+    brush.clear();
+    canvas.begin();
+    ofClear(0,0);
+    canvas.end();
+}
+
+void CloudsVisualSystemPaintBrush::selfSetupGuis()
+{
+    
+}
+
+void CloudsVisualSystemPaintBrush::selfAutoMode()
+{
+    
+}
+
+
+void CloudsVisualSystemPaintBrush::selfDrawBackground()
+{
+    
+}
+
+void CloudsVisualSystemPaintBrush::selfDrawDebug()
+{
+    
+}
+
+void CloudsVisualSystemPaintBrush::selfSceneTransformation()
+{
+    
+}
+
+void CloudsVisualSystemPaintBrush::selfExit()
+{
+    
+}
+
+void CloudsVisualSystemPaintBrush::selfBegin()
+{
+    
+}
+
+void CloudsVisualSystemPaintBrush::selfEnd()
+{
+    
+}
+
+void CloudsVisualSystemPaintBrush::selfKeyReleased(ofKeyEventArgs & args)
+{
+    
+}
+
+void CloudsVisualSystemPaintBrush::mouseDragged(ofMouseEventArgs& data)
+{
+    if ( palette.getVisible() ){
+        brush.pickColorFrom( palette.getTextureReference(),colorLerp,colorRandom );
+    }
+    brush.set(data.x,data.y);
+}
+
+void CloudsVisualSystemPaintBrush::mouseMoved(ofMouseEventArgs &args)
+{
+    
+}
+
+void CloudsVisualSystemPaintBrush::mousePressed(ofMouseEventArgs &args)
+{
+    ofPoint mouse(args.x,args.y);
+    
+    if ( !cleanBtn.checkOver(mouse) && !paletteBtn.checkOver(mouse)){
+        
+        if (palette.getVisible()){
+            if (  mouse.y > palette.getY() ){
+                brush.clear();
+                palette.setVisible(false);
+            }
+        }
+        
+        paletteBtn.checkOver(mouse);
+        cleanBtn.checkOver(mouse);
+        
+        brush.init(brushNumber);
+        brush.setBrushWidth(brushWidth);
+        brush.setLineWidth(lineWidth);
+        
+        brush.damp = brushDamp;
+        brush.k = brushK;
+        brush.repPct = brushRepPct;
+        brush.repRad = brushRepRad;
+        
+        brush.begin();
+        brush.set(mouse);
+    }
+}
+
+void CloudsVisualSystemPaintBrush::mouseReleased(ofMouseEventArgs &args)
+{
+    brush.set(args.x, args.y);
+    brush.end();
+}
+
+void CloudsVisualSystemPaintBrush::selfSetupGui()
+{
+    
+}
+
+void CloudsVisualSystemPaintBrush::selfGuiEvent(ofxUIEventArgs &e)
+{
+    
+}
+
+void CloudsVisualSystemPaintBrush::guiRenderEvent(ofxUIEventArgs &e)
+{
+    
+}
--- CloudsLibrary/src/VisualSystems/CloudsVisualSystemPaintBrush.h
+++ CloudsLibrary/src/VisualSystems/CloudsVisualSystemPaintBrush.h
@@ -6,9 +6,76 @@
 //
 //
 
-#ifndef __VSPaintBrush__CloudsVisualSystemPaintBrush__
-#define __VSPaintBrush__CloudsVisualSystemPaintBrush__
+#pragma once
 
-#include <iostream>
+#include "CloudsVisualSystemRezanator.h"
 
-#endif /* defined(__VSPaintBrush__CloudsVisualSystemPaintBrush__) */
+#include "Brush.h"
+#include "ColorPalette.h"
+#include "Button.h"
+
+class CloudsVisualSystemPaintBrush : public CloudsVisualSystemRezanator {
+public:
+    
+    string getSystemName();
+    
+    void selfSetup();
+    void selfSetupGuis();
+    
+    void selfAutoMode();
+    void selfUpdate();
+    void selfDrawBackground();
+    void selfDrawDebug();
+    void selfSceneTransformation();
+    
+    void draw(ofEventArgs & args);
+    
+    void selfExit();
+    void selfBegin();
+	void selfEnd();
+    
+    void selfKeyPressed(ofKeyEventArgs & args);
+    void selfKeyReleased(ofKeyEventArgs & args);
+    
+    void mouseDragged(ofMouseEventArgs & args);
+    void mouseMoved(ofMouseEventArgs & args);
+    void mousePressed(ofMouseEventArgs & args);
+    void mouseReleased(ofMouseEventArgs & args);
+    
+    void selfSetupGui();
+    void selfGuiEvent(ofxUIEventArgs &e);
+    
+    void selfSetupSystemGui();
+    void guiSystemEvent(ofxUIEventArgs &e);
+    
+    void selfSetupRenderGui();
+    void guiRenderEvent(ofxUIEventArgs &e);
+    
+protected:
+    
+    void showPalette(int &_n );
+    void cleanCanvas(int &_n );
+    
+    float   brushWidth;
+    float   brushNumber;
+    float   brushDamp;
+    float   brushK;
+    float   brushRepRad;
+    float   brushRepPct;
+    float   lineWidth;
+    float   colorLerp;
+    float   colorRandom;
+    
+    Brush           brush;
+    ofFbo           canvas;
+    
+    ColorPalette    palette;
+    
+    SimpleBtn       paletteBtn;
+    SimpleBtn       cleanBtn;
+    
+    int             iconSize;
+    
+    bool            bDebug;
+    
+};
