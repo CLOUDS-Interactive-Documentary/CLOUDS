d9d6427df4727447c74180ea7b84535e1bd83888
Surya Mattu <surya@suryamattu.com>
2013-06-24 20:25:17 -0400

    added functionality to linker

--- CloudsFCPParser/src/ClipSelector.h
+++ CloudsFCPParser/src/ClipSelector.h
@@ -7,7 +7,58 @@
 //
 
 #import <Foundation/Foundation.h>
+#include "CloudsFCPParser.h"
+@class testView;
 
-@interface ClipSelector : NSObject
+@interface ClipSelector : NSObject <NSTableViewDataSource, NSTableViewDelegate, NSTokenFieldDelegate> {
+
+    IBOutlet NSTableView* keywordTable;
+    IBOutlet NSTableView* clipTable;
+    IBOutlet NSTableView* linkTable;
+    IBOutlet NSTableView* suppressedTable;
+    IBOutlet NSTokenField* currentKeywords;
+
+    IBOutlet NSTextField* startQuestion;
+    IBOutlet NSTextField* currentClipLabel;
+    IBOutlet NSTextField* linkText;
+    IBOutlet NSButton* showOnlyQuestions;
+    
+    vector<CloudsClip> selectedClips;
+    vector<string> selectedKeywords;
+    
+    vector<CloudsLink> currentClipLinks;
+    vector<CloudsLink> currentSuppressedLinks;
+    CloudsFCPParser* parser;
+    testView* testViewParent;
+    
+    
+    
+}
+
+ -(void)setup;
+- (void) playClip:(CloudsClip&) clip;
+
+@property(nonatomic, readwrite) CloudsFCPParser* parser;
+@property(nonatomic, assign) testView* testViewParent;
+
+- (IBAction) playDoubleClickedRow:(id)sender;
+- (IBAction) createLink:(id)sender;
+- (IBAction) suppressLink:(id)sender;
+- (CloudsClip&) selectedClip;
+- (IBAction) createLink:(id)sender;
+- (IBAction) deleteLink:(id)sender;
+- (void)keyPressed:(int)key;
+-(IBAction)setStartText:(id)sender;
+- (IBAction) saveLinks:(id)sender;
+- (IBAction)suppressLinkModifier:(id)sender;
+- (IBAction) showQuestiosnChanged:(id)sender;
+- (BOOL)tableView:(NSTableView *)aTableView shouldEditTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex;
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)aTableView;
+- (id)tableView:(NSTableView *)aTableView objectValueForTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex;
+- (void)tableViewSelectionDidChange:(NSNotification *)aNotification;
+- (void)tableView:(NSTableView *)tableView sortDescriptorsDidChange: (NSArray *)oldDescriptors;
+- (NSArray *)tokenField:(NSTokenField *)tokenField completionsForSubstring:(NSString *)substring indexOfToken:(NSInteger)tokenIndex indexOfSelectedItem:(NSInteger *)selectedIndex;
+
 
 @end
