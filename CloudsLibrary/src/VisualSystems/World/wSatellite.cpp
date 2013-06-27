9b86c9e0b01e59fabaebc6a2a11cc71685889a7b
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2013-06-27 16:06:30 -0400

    VSWorld

---
+++ CloudsLibrary/src/VisualSystems/World/wSatellite.cpp
@@ -0,0 +1,9 @@
+//
+//  wSatellite.cpp
+//  VSWorld
+//
+//  Created by Patricio Gonzalez Vivo on 6/27/13.
+//
+//
+
+#include "wSatellite.h"
---
+++ CloudsLibrary/src/VisualSystems/World/wSatellite.h
@@ -0,0 +1,14 @@
+//
+//  wSatellite.h
+//  VSWorld
+//
+//  Created by Patricio Gonzalez Vivo on 6/27/13.
+//
+//
+
+#ifndef __VSWorld__wSatellite__
+#define __VSWorld__wSatellite__
+
+#include <iostream>
+
+#endif /* defined(__VSWorld__wSatellite__) */
