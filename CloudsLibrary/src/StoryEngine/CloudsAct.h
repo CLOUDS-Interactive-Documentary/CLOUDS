9dc794e52aa182831693658802efae10d6938e26
Surya Mattu <surya@suryamattu.com>
2013-07-04 20:09:54 -0400

    clouds act - more stuff

--- CloudsLibrary/src/StoryEngine/CloudsAct.h
+++ CloudsLibrary/src/StoryEngine/CloudsAct.h
@@ -8,7 +8,42 @@
 
 #ifndef __CloudsStoryEngine__CloudsAct__
 #define __CloudsStoryEngine__CloudsAct__
+#include "ofMain.h"
+#include "CloudsEvents.h"
+#include "CloudsFCPParser.h"
+#include "CloudsVisualSystemManager.h"
+#include "CloudsEvents.h"
 
-#include <iostream>
+class CloudsAct{
+  
+    public:
+    CloudsAct();
+    vector<CloudsClip> clips;
+    vector<CloudsVisualSystemPreset> visualSystems;
+    vector<string> questions;
+    
+    
+    void update(); //you can use this to skip if it's waiting
+    void playAct();
+
+    
+	bool clipEnded(); //call this when the clip is done!
+    void drawActDebug();
+
+    
+    CloudsEvents& getEvents();
+    
+protected:
+    CloudsClip currentClip;
+    CloudsEvents events;
+    
+    bool actPlaying;
+    float currentTime;
+    float clipEndTime;
+
+    int currentPlayIndex;
+    void playNextClip();
+    float getActDuration();
+};
 
 #endif /* defined(__CloudsStoryEngine__CloudsAct__) */
