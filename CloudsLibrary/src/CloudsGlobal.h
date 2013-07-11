8b78f1897a2b5959c9cf4044f9c5a23f32ea0d91
James George <james@jamesgeorge.org>
2013-07-11 13:57:55 -0400

    added clone add ons for av foundation

---
+++ CloudsLibrary/src/CloudsGlobal.h
@@ -0,0 +1,14 @@
+//
+//  CloudsGlobal.h
+//  CloudsStoryEngine
+//
+//  Created by James George on 7/11/13.
+//
+//
+
+#ifndef CloudsStoryEngine_CloudsGlobal_h
+#define CloudsStoryEngine_CloudsGlobal_h
+
+
+
+#endif
