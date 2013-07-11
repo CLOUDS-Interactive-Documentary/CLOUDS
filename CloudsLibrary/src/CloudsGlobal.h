f6c00e1fe2c1907c3e6297f612a2d669ead9ed72
James George <james@jamesgeorge.org>
2013-07-11 13:59:44 -0400

    global static include for file tasks

--- CloudsLibrary/src/CloudsGlobal.h
+++ CloudsLibrary/src/CloudsGlobal.h
@@ -9,6 +9,48 @@
 #ifndef CloudsStoryEngine_CloudsGlobal_h
 #define CloudsStoryEngine_CloudsGlobal_h
 
+#include "ofMain.h"
 
+static bool confirmedDataPath = false;
+static bool usingDevelopmentFolder = false;
+
+//--------------------------------------------------------------------
+static string getDataPath()
+{
+	if(!confirmedDataPath){
+		usingDevelopmentFolder = ofDirectory("../../../CloudsData/").exists();
+		if(!usingDevelopmentFolder){
+			ofDirectory("CloudsData/").create();
+		}
+		confirmedDataPath = true;
+	}
+    return usingDevelopmentFolder ? "../../../CloudsData/" : "CloudsData/";
+}
+
+//--------------------------------------------------------------------
+static string relinkFilePath(string filePath){
+	
+	vector<string> drives;
+	
+	drives.push_back("Seance");
+	drives.push_back("Nebula");
+	drives.push_back("Supernova");
+	drives.push_back("Nebula_helper");
+    
+	if( !ofFile(filePath).exists() ){
+		for(int i = 0; i < drives.size(); i++){
+			if(ofFile::doesFileExist("/Volumes/"+ drives[i]+"/")){
+				for(int j = 0; j < drives.size(); j++){
+					if(j != i){
+						ofStringReplace(filePath, drives[j], drives[i]);
+					}
+				}
+				break;
+			}
+		}
+	}
+	
+	return filePath;
+}
 
 #endif
