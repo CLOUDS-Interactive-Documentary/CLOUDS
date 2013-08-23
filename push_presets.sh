eac569f0932f12ac7b54755fa83e87e88dd8b90f
James George <james@jamesgeorge.org>
2013-08-23 15:39:01 -0700

    update all sub modules

--- push_presets.sh
+++ push_presets.sh
@@ -1,17 +1,24 @@
 #/bin/bash
 
-VS_DIR=CloudsLibrary/src/VisualSystems
-cd $VS_DIR
+cd CloudsLibrary/src/VisualSystemsLibrary
+git checkout master
+git add *.cpp *.h
+git commit -m "auto commit changes"
+git push origin master
+cd ../../../
+
+cd CloudsLibrary/src/VisualSystems
+
 for i in `ls .` ; do
 
   if [ -e "$i/.git" ]; then
 	echo "UPDATING $i"
 	cd $i
 	git checkout master
-    git add *.xml *.cpp *.h
+    git add *
+  	git commit -m "updating presets"
     git pull
 	if [ $? -eq 0 ]; then
-  	  git commit -m "updating presets"
 	  git push
  	fi
     cd ..
