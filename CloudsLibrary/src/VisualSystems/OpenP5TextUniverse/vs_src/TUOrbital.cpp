9f8ac1918919e55349d61392eebeaf0c1428d0cb
Elie Zananiri <prisonerjohn@gmail.com>
2013-10-29 15:42:28 -0400

    VSOpenP5TextUniverse: Funky text nodes, needs some FTGL.

---
+++ CloudsLibrary/src/VisualSystems/OpenP5TextUniverse/vs_src/TUOrbital.cpp
@@ -0,0 +1,13 @@
+//
+//  TUOrbital.cpp
+//  OpenP5TextUniverse
+//
+//  Created by Elie Zananiri on 2013-10-29.
+//
+//
+
+#include "TUOrbital.h"
+
+float TUOrbital::focusX;
+float TUOrbital::focusY;
+float TUOrbital::focusZ;
