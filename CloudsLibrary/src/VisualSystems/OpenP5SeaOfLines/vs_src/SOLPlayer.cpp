7a828d3e10be8f42474ab2fc7577c3746626c485
Elie Zananiri <prisonerjohn@gmail.com>
2013-11-05 16:48:54 -0500

    OpenP5SeaOfLines: Cleaned up.

--- CloudsLibrary/src/VisualSystems/OpenP5SeaOfLines/vs_src/SOLPlayer.cpp
+++ CloudsLibrary/src/VisualSystems/OpenP5SeaOfLines/vs_src/SOLPlayer.cpp
@@ -51,33 +51,4 @@ void SOLPlayer::update()
         y = ofGetHeight() - size / 2;
         sy *= -1;
     }
-    
-    
-    
-//    //  fill(c);
-//    x+=vx;y+=vy;vy+=0.1;
-//    if(x<taille/2){x=taille/2;vx=-vx;}
-//    if(y<taille/2){y=taille/2;vy=-vy;}
-//    if(x>width-taille/2){x=width-taille/2;vx=-vx;}
-//    if(y>height-taille/2){y=height-taille/2;vy=-vy*0.75;}
-//    for(int a=0;a<personnages.length;a++){
-//        people deux = personnages[a];
-//        if(deux!=this){
-//            float d=dist(deux.x, deux.y, x , y);
-//            if(d<((taille+deux.taille)/2)){
-//                float ang = atan2(y-deux.y, x-deux.x);
-//                vx = cos(ang)*vitesse;vy=sin(ang)*vitesse;
-//            } else {
-//                if(d<30){
-//                    //stroke(255);
-//                    line(x,y,deux.x, deux.y);
-//                    
-//                }
-//                
-//            }
-//            
-//        }
-//    }
-//    //  ellipse(x,y,taille,taille);
-    
 }
