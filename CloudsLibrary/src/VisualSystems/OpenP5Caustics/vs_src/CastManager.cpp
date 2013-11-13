6ce27e9caed0fc0a33df7ede5a2d07e1c84e8f9b
Elie Zananiri <prisonerjohn@gmail.com>
2013-11-13 15:29:43 -0500

    OpenP5Caustics: First version!

---
+++ CloudsLibrary/src/VisualSystems/OpenP5Caustics/vs_src/CastManager.cpp
@@ -0,0 +1,128 @@
+//
+//  CastManager.cpp
+//  OpenP5Caustics
+//
+//  Created by Elie Zananiri on 11/13/2013.
+//
+//
+
+#include "CastManager.h"
+
+//--------------------------------------------------------------
+//--------------------------------------------------------------
+CastManager::CastManager(int width, int height)
+{
+    this->width = width;
+    this->height = height;
+
+    cR = new Caster(width, height);
+    cG = new Caster(width, height);
+    cB = new Caster(width, height);
+    
+    bump = 0.5;
+    noiseScale = 0.008;
+    boxW = 300;
+    boxH = 300;
+    z = 0;
+    bReset = false;
+    bReseed = false;
+    loopCount;
+    fpsGranularity = 10000;
+    algo = kCastAlgoSpiral;
+}
+
+//--------------------------------------------------------------
+//--------------------------------------------------------------
+CastManager::~CastManager()
+{
+    delete cR;
+    delete cG;
+    delete cB;
+}
+
+//--------------------------------------------------------------
+//--------------------------------------------------------------
+void CastManager::threadedFunction()
+{
+    long timeStart = ofGetElapsedTimeMillis();
+    
+    float r = 10;
+    float t = 0;
+    int rDir = 1;
+    
+    while (true) {
+        if (loopCount % fpsGranularity == 0) {
+            long delta = ofGetElapsedTimeMillis() - timeStart;
+            if (delta != 0) {
+                fps = fpsGranularity*1000.f / delta;
+            }
+            timeStart = ofGetElapsedTimeMillis();
+        }
+        
+        if (bReseed) {
+            ofSeedRandom(ofGetElapsedTimeMillis());
+            bReseed = false;
+        }
+        
+        if (bReset) {
+            cR->reset();
+            cG->reset();
+            cB->reset();
+            bReset = false;
+        }
+        
+        float x, y;
+        switch (algo) {
+            case kCastAlgoRandom:
+                x = ofRandom(boxW) + width  / 2 - boxW / 2;
+                y = ofRandom(boxH) + height / 2 - boxH / 2;
+                break;
+                
+            case kCastAlgoSpiral:
+            case kCastAlgoSpiralClear:
+                r = (r + 0.0001f * rDir);
+                if (ABS(r) > boxW / 2) {
+                    rDir *= -1;
+                }
+                t = fmodf(t + 0.001f, TWO_PI);
+                x = r * cosf(t) + width  / 2;
+                y = r * sinf(t) + height / 2;
+                break;
+                
+            default:
+                x = 0; y = 0;
+                ofLogError("CastManager::threadedFunction()") << "Unknown algo: " << algo;
+        }
+        
+        // Calculation of gradient.
+        float dzdx = (ofNoise(x * noiseScale + noiseScale, y * noiseScale, z * noiseScale) - ofNoise(x * noiseScale - noiseScale, y * noiseScale, z * noiseScale)) * bump / (noiseScale * 2);
+        float dzdy = (ofNoise(x * noiseScale, y * noiseScale + noiseScale, z * noiseScale) - ofNoise(x * noiseScale, y * noiseScale - noiseScale, z * noiseScale)) * bump / (noiseScale * 2);
+        
+        // More accurate calculation of gradient.
+        //float dzdx = (ofNoise(x * noiseScale - 2 * noiseScale, y * noiseScale, z * noiseScale) * -0.5
+        //            + ofNoise(x * noiseScale - noiseScale, y * noiseScale, z * noiseScale) * -1
+        //            + ofNoise(x * noiseScale + noiseScale, y * noiseScale, z * noiseScale) *  1
+        //            + ofNoise(x * noiseScale + 2 * noiseScale, y * noiseScale, z * noiseScale) *  0.5) * bump / (noiseScale * 2);
+        
+        //float dzdy = (ofNoise(x * noiseScale, y * noiseScale - 2 * noiseScale, z * noiseScale) * -0.5
+        //            + ofNoise(x * noiseScale, y * noiseScale - noiseScale, z * noiseScale) * -1
+        //            + ofNoise(x * noiseScale, y * noiseScale + noiseScale, z * noiseScale) *  1
+        //            + ofNoise(x * noiseScale, y * noiseScale + 2 * noiseScale, z * noiseScale) *  0.5) * bump / (noiseScale * 2);
+        
+        float m = ofVec3f(dzdx, dzdy, 1).length();
+        
+        // Normal to surface.
+        float nX = dzdx / m;
+        float nY = dzdy / m;
+        float nZ = 1 / m;
+
+        float thetaI = acosf(nZ);
+        
+        cR->cast(x, y, nX, nY, nZ, thetaI);
+        cG->cast(x, y, nX, nY, nZ, thetaI);
+        cB->cast(x, y, nX, nY, nZ, thetaI);
+        
+        loopCount++;
+//        ofSleepMillis(20);
+    }
+}
---
+++ CloudsLibrary/src/VisualSystems/OpenP5Caustics/vs_src/CloudsVisualSystemOpenP5Caustics.cpp
@@ -0,0 +1,264 @@
+//
+//  CloudsVisualSystemOpenP5Caustics.cpp
+//
+
+#include "CloudsVisualSystemOpenP5Caustics.h"
+
+//#include "CloudsRGBDVideoPlayer.h"
+//#ifdef AVF_PLAYER
+//#include "ofxAVFVideoPlayer.h"
+//#endif
+
+//--------------------------------------------------------------
+void CloudsVisualSystemOpenP5Caustics::selfSetupGui()
+{
+	customGui = new ofxUISuperCanvas("CAUSTICS", gui);
+	customGui->copyCanvasStyle(gui);
+	customGui->copyCanvasProperties(gui);
+	customGui->setName("Caustics");
+	customGui->setWidgetFontSize(OFX_UI_FONT_SMALL);
+    
+    customGui->addSpacer();
+    customGui->addButton("CLEAR", &bClear);
+    customGui->addButton("RESET", &bReset);
+    
+    customGui->addSpacer();
+    customGui->addSlider("R INTENSITY", 0, 100, &castManager->cR->intensity);
+    customGui->addSlider("G INTENSITY", 0, 100, &castManager->cG->intensity);
+    customGui->addSlider("B INTENSITY", 0, 100, &castManager->cB->intensity);
+    
+    customGui->addSpacer();
+    customGui->addSlider("NOISE SCALE", 0, 50, castManager->noiseScale * 1500.0f);
+    customGui->addSlider("BOX WIDTH", 0, MAX(ofGetScreenWidth(), ofGetScreenHeight()), &castManager->boxW);
+    customGui->addSlider("BOX HEIGHT", 0, MAX(ofGetScreenWidth(), ofGetScreenHeight()), &castManager->boxH);
+    customGui->addSlider("DEPTH", 1, 200, 100);
+    customGui->addSlider("IOR", 1.0f, 3.0f, 1.3f);
+    customGui->addSlider("IOR DIVERGENCE", 0.0f, 0.2f, nDiv);
+    
+    customGui->addSpacer();
+    vector<string> modes;
+    modes.push_back("SPIRAL");
+    modes.push_back("SPIRAL CLEAR");
+    modes.push_back("RANDOM");
+    customGui->addRadio("EMIT MODE", modes);
+    
+	ofAddListener(customGui->newGUIEvent, this, &CloudsVisualSystemOpenP5Caustics::selfGuiEvent);
+	guis.push_back(customGui);
+	guimap[customGui->getName()] = customGui;
+}
+
+//--------------------------------------------------------------
+void CloudsVisualSystemOpenP5Caustics::selfGuiEvent(ofxUIEventArgs &e)
+{
+    if (e.widget->getName() == "NOISE SCALE") {
+        castManager->noiseScale = ((ofxUISlider *)e.widget)->getScaledValue() / 1500.0f;
+    }
+    
+    else if (e.widget->getName() == "SPIRAL") {
+        castManager->algo = kCastAlgoSpiral;
+    }
+    else if (e.widget->getName() == "SPIRAL CLEAR") {
+        castManager->algo = kCastAlgoSpiralClear;
+    }
+    else if (e.widget->getName() == "RANDOM") {
+        castManager->algo = kCastAlgoRandom;
+    }
+
+    else if (e.widget->getName() == "DEPTH") {
+        castManager->cR->d = castManager->cG->d = castManager->cB->d = ((ofxUISlider *)e.widget)->getScaledValue();
+    }
+    else if (e.widget->getName() == "IOR") {
+        float val = ((ofxUISlider *)e.widget)->getScaledValue();
+        castManager->cR->n = val;
+        castManager->cG->n = val + nDiv;
+        castManager->cB->n = val + nDiv * 2;
+    }
+    else if (e.widget->getName() == "IOR DIVERGENCE") {
+        float val = castManager->cR->n;
+        castManager->cG->n = val + nDiv;
+        castManager->cB->n = val + nDiv * 2;
+    }
+    
+    if (e.widget->getName() == "R INTENSITY" ||
+        e.widget->getName() == "G INTENSITY" ||
+        e.widget->getName() == "B INTENSITY" ||
+        e.widget->getName() == "NOISE SCALE" ||
+        e.widget->getName() == "DEPTH" ||
+        e.widget->getName() == "IOR" ||
+        e.widget->getName() == "IOR DIVERGENCE") {
+        clear();
+    }
+}
+
+//Use system gui for global or logical settings, for exmpl
+void CloudsVisualSystemOpenP5Caustics::selfSetupSystemGui(){
+	
+}
+
+void CloudsVisualSystemOpenP5Caustics::guiSystemEvent(ofxUIEventArgs &e){
+	
+}
+//use render gui for display settings, like changing colors
+void CloudsVisualSystemOpenP5Caustics::selfSetupRenderGui(){
+
+}
+
+void CloudsVisualSystemOpenP5Caustics::guiRenderEvent(ofxUIEventArgs &e){
+	
+}
+
+//--------------------------------------------------------------
+void CloudsVisualSystemOpenP5Caustics::selfSetup()
+{
+    width = ofGetWidth();
+    height = ofGetHeight();
+    numPixels = width * height;
+    
+    outputTexture.allocate(width, height, GL_RGB);
+    outputPixels = new unsigned char[numPixels * 3];
+    for (int i = 0; i < numPixels; i++) {
+        outputPixels[i * 3 + 0] = 0;  // R
+        outputPixels[i * 3 + 1] = 0;  // G
+        outputPixels[i * 3 + 2] = 0;  // B
+    }
+    castManager = new CastManager(width, height);
+    castManager->start();
+    
+    nDiv = 0.02;
+    bReset = false;
+    bClear = false;
+}
+
+//--------------------------------------------------------------
+void CloudsVisualSystemOpenP5Caustics::reset()
+{
+    castManager->reseed();
+    castManager->reset();
+}
+
+//--------------------------------------------------------------
+void CloudsVisualSystemOpenP5Caustics::clear()
+{
+    castManager->reset();
+}
+
+//--------------------------------------------------------------
+void CloudsVisualSystemOpenP5Caustics::selfPresetLoaded(string presetPath)
+{
+
+}
+
+// selfBegin is called when the system is ready to be shown
+// this is a good time to prepare for transitions
+// but try to keep it light weight as to not cause stuttering
+void CloudsVisualSystemOpenP5Caustics::selfBegin(){
+	
+}
+
+//do things like ofRotate/ofTranslate here
+//any type of transformation that doesn't have to do with the camera
+void CloudsVisualSystemOpenP5Caustics::selfSceneTransformation(){
+	
+}
+
+//--------------------------------------------------------------
+void CloudsVisualSystemOpenP5Caustics::selfUpdate()
+{
+    if (bClear || castManager->algo == kCastAlgoSpiralClear) {
+        clear();
+        bClear = false;
+    }
+    
+    if (bReset) {
+        reset();
+        bReset = false;
+    }
+    
+    float sum = 0;
+    float maxV = 0;
+    float count = 0;
+    for (int i = 0; i < castManager->cR->count; i++) {
+        float m = MAX(castManager->cR->bg[i], MAX(castManager->cG->bg[i], castManager->cB->bg[i]));
+        sum += m;
+        maxV = (maxV > m)? maxV : m;
+        if (m!=0) count++;
+    }
+    float avg = sum / count;
+
+    for (int i = 0; i < numPixels; i++) {
+        if (ofGetKeyPressed('n')) {
+            int c = ofNoise(i % width * castManager->noiseScale, i / width * castManager->noiseScale, castManager->z * castManager->noiseScale) * 255;
+            outputPixels[i * 3 + 0] = c;  // R
+            outputPixels[i * 3 + 1] = c;  // G
+            outputPixels[i * 3 + 2] = c;  // B
+        }
+        else {
+            outputPixels[i * 3 + 0] = MAX(0, MIN(255, castManager->cR->bg[i] * 128 / avg));  // R
+            outputPixels[i * 3 + 1] = MAX(0, MIN(255, castManager->cG->bg[i] * 128 / avg));  // G
+            outputPixels[i * 3 + 2] = MAX(0, MIN(255, castManager->cB->bg[i] * 128 / avg));  // B
+        }
+    }
+
+    outputTexture.loadData(outputPixels, width, height, GL_RGB);
+}
+
+//--------------------------------------------------------------
+void CloudsVisualSystemOpenP5Caustics::selfDraw()
+{
+
+}
+
+// draw any debug stuff here
+void CloudsVisualSystemOpenP5Caustics::selfDrawDebug(){
+
+}
+
+//--------------------------------------------------------------
+void CloudsVisualSystemOpenP5Caustics::selfDrawBackground()
+{
+    ofSetColor(255);
+    outputTexture.draw(0, 0);
+}
+
+// this is called when your system is no longer drawing.
+// Right after this selfUpdate() and selfDraw() won't be called any more
+void CloudsVisualSystemOpenP5Caustics::selfEnd(){
+
+	
+}
+
+void CloudsVisualSystemOpenP5Caustics::selfExit()
+{
+    castManager->stop();
+    delete castManager;
+    
+    numPixels = 0;
+    delete [] outputPixels;
+    
+    outputTexture.clear();
+}
+
+//events are called when the system is active
+//Feel free to make things interactive for you, and for the user!
+void CloudsVisualSystemOpenP5Caustics::selfKeyPressed(ofKeyEventArgs & args){
+	
+}
+void CloudsVisualSystemOpenP5Caustics::selfKeyReleased(ofKeyEventArgs & args){
+	
+}
+
+void CloudsVisualSystemOpenP5Caustics::selfMouseDragged(ofMouseEventArgs& data){
+
+}
+
+void CloudsVisualSystemOpenP5Caustics::selfMouseMoved(ofMouseEventArgs& data){
+	
+}
+
+void CloudsVisualSystemOpenP5Caustics::selfMousePressed(ofMouseEventArgs& data){
+    
+}
+
+void CloudsVisualSystemOpenP5Caustics::selfMouseReleased(ofMouseEventArgs& data){
+
+}
