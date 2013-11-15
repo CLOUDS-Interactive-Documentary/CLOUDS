eecfe0e932cf3ac6ef4a35cc05b11dd851c2b4d2
jonathanminard <jonathanminard@gmail.com>
2013-11-15 14:54:45 -0500

    Vision presets

--- CloudsLibrary/src/VisualSystems/Vision/vs_src/CloudsVisualSystemVision.cpp
+++ CloudsLibrary/src/VisualSystems/Vision/vs_src/CloudsVisualSystemVision.cpp
@@ -86,7 +86,7 @@ void CloudsVisualSystemVision::selfSetup()
     movieStrings.push_back("indianTrafficCrop.mov");
     movieStrings.push_back("unionsq_1 - Wi-Fi_crop.mov");
     movieStrings.push_back("Swarm_EindhovenTest_Watec_two-visitors_Crop.mov");
-  /*
+ 
     movieStrings.push_back("AppleStore1_short.mov");
     movieStrings.push_back("AppleStore2_short.mov");
     movieStrings.push_back("GreenPoint_bike_crop.mov");
@@ -113,7 +113,7 @@ void CloudsVisualSystemVision::selfSetup()
     movieStrings.push_back("Tokyo1.mov");
     movieStrings.push_back("Tokyo2.mov");
     movieStrings.push_back("TokyoEscalator.mov");
-*/
+
     frameIsNew = false;
     window = ofRectangle(0,0,500,500);
     loadCurrentMovie();
