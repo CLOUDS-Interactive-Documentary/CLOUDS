d1f8434123242925d3e3dc4e7659ca88b2d52802
obviousjim <james@jamesgeorge.org>
2013-12-08 19:24:32 -0500

    Added KinectOSC Input

---
+++ CloudsLibrary/src/Input/CloudsInputKinectOSC.cpp
@@ -0,0 +1,171 @@
+//
+//  CloudsInputKinectOSC.cpp
+//  YellowTail
+//
+//  Created by Elie Zananiri on 12/8/13.
+//  
+//
+
+#include "CloudsInputKinectOSC.h"
+
+int kListenPort          = 12345;
+int kNumFramesForRemoval = 60;
+
+//--------------------------------------------------------------
+void CloudsInputKinectOSC::enable(){
+	if(!enabled){
+        receiver.setup(kListenPort);
+        ofAddListener(ofEvents().update, this, &CloudsInputKinectOSC::update);
+		enabled = true;
+	}
+}
+
+//--------------------------------------------------------------
+void CloudsInputKinectOSC::disable(){
+	if(enabled){
+//        receiver.shutdown();
+        ofRemoveListener(ofEvents().update, this, &CloudsInputKinectOSC::update);
+		enabled = false;
+	}
+}
+
+//--------------------------------------------------------------
+void CloudsInputKinectOSC::update(ofEventArgs& args){
+	// check for waiting messages
+    while (receiver.hasWaitingMessages()) {
+        lastOscFrame = ofGetFrameNum();
+        
+		// get the next message
+		ofxOscMessage m;
+		receiver.getNextMessage(&m);
+        
+        bool bRecognized = true;
+        
+        k4w::HandState newHandState;
+        
+		if (m.getAddress() == "/playerData") {
+            int i = 0;
+			int idx = m.getArgAsInt32(i++);
+            
+            cout << "RECEIVED idx " << idx << " on frame " << lastOscFrame << endl;
+            
+            // create a body if it doesn't exist yet
+            if (bodies.find(idx) == bodies.end()) {
+                bodies[idx] = new k4w::Body();
+                bodies[idx]->idx = idx;
+            }
+            
+            // update the head joint
+            bodies[idx]->headJoint.type = (k4w::JointType)m.getArgAsInt32(i++);
+            bodies[idx]->headJoint.trackingState = (k4w::TrackingState)m.getArgAsInt32(i++);
+            bodies[idx]->headJoint.position.set(m.getArgAsFloat(i++), 
+                                                m.getArgAsFloat(i++), 
+                                                m.getArgAsFloat(i++));
+            
+            // update the left hand joint
+            bodies[idx]->leftHandJoint.type = (k4w::JointType)m.getArgAsInt32(i++);
+            bodies[idx]->leftHandJoint.trackingState = (k4w::TrackingState)m.getArgAsInt32(i++);
+            bodies[idx]->leftHandJoint.position.set(m.getArgAsFloat(i++), 
+                                                    m.getArgAsFloat(i++), 
+                                                    m.getArgAsFloat(i++));
+            newHandState = (k4w::HandState)m.getArgAsInt32(i++);
+            processHandEvent(bodies[idx]->leftHandJoint.handState, newHandState, idx, bodies[idx]->leftHandJoint.position);
+            bodies[idx]->leftHandJoint.handState = newHandState;
+            
+            // update the right hand joint
+            bodies[idx]->rightHandJoint.type = (k4w::JointType)m.getArgAsInt32(i++);
+            bodies[idx]->rightHandJoint.trackingState = (k4w::TrackingState)m.getArgAsInt32(i++);
+            bodies[idx]->rightHandJoint.position.set(m.getArgAsFloat(i++), 
+                                                     m.getArgAsFloat(i++), 
+                                                     m.getArgAsFloat(i++));
+            newHandState = (k4w::HandState)m.getArgAsInt32(i++);
+            processHandEvent(bodies[idx]->rightHandJoint.handState, newHandState, idx, bodies[idx]->rightHandJoint.position);
+            bodies[idx]->rightHandJoint.handState = newHandState;
+            
+            // refresh the update frame
+            bodies[idx]->lastUpdateFrame = lastOscFrame;
+		}
+		else {
+            bRecognized = false;
+        }
+        
+        if (!bRecognized) {
+			// display in the console
+			string msg_string;
+			msg_string = m.getAddress();
+			msg_string += ": ";
+			for (int i = 0; i < m.getNumArgs(); i++) {
+				// get the argument type
+				msg_string += m.getArgTypeName(i);
+				msg_string += ":";
+				// display the argument - make sure we get the right type
+				if (m.getArgType(i) == OFXOSC_TYPE_INT32) {
+					msg_string += ofToString(m.getArgAsInt32(i));
+				}
+				else if (m.getArgType(i) == OFXOSC_TYPE_FLOAT) {
+					msg_string += ofToString(m.getArgAsFloat(i));
+				}
+				else if (m.getArgType(i) == OFXOSC_TYPE_STRING) {
+					msg_string += m.getArgAsString(i);
+				}
+				else {
+					msg_string += "unknown";
+				}
+                msg_string += " ";
+			}
+            ofLog() << msg_string;
+		}
+	}
+    
+    // remove any dead bodies
+    vector<int> toRemove;
+    for (map<int, k4w::Body *>::iterator it = bodies.begin(); it != bodies.end(); ++it) {
+        if (ABS(it->second->lastUpdateFrame - lastOscFrame) > kNumFramesForRemoval) {
+            cout << "REMOVING idx " << it->first << " frame " << it->second->lastUpdateFrame << " vs " << lastOscFrame << endl;
+            toRemove.push_back(it->first);
+        }
+    }
+    for (int i = 0; i < toRemove.size(); i++) {
+        delete bodies[toRemove[i]];
+        bodies.erase(toRemove[i]);
+    }
+}
+
+void CloudsInputKinectOSC::processHandEvent(k4w::HandState prevState, k4w::HandState currState, int bodyIdx, ofVec3f& position){
+    // temp mapping
+    ofVec3f position2D(ofMap(position.x, -1.0, 1.0, 0, ofGetWidth()),
+                       ofMap(position.y, -1.0, 1.0, 0, ofGetHeight()),
+                       ofMap(position.z, -1.0, 1.0, 0, ofGetWidth()));
+    
+	if (prevState == k4w::HandState_Open && currState == k4w::HandState_Lasso) {
+        CloudsInteractionEventArgs args(position2D, 0, bodyIdx);
+        ofNotifyEvent(events.interactionStarted, args, this);
+    }    
+    else if (prevState == k4w::HandState_Open && currState == k4w::HandState_Closed) {
+        CloudsInteractionEventArgs args(position2D, 2, bodyIdx);
+        ofNotifyEvent(events.interactionStarted, args, this);
+    }
+    
+    else if (prevState == k4w::HandState_Lasso && currState == k4w::HandState_Open) {
+        CloudsInteractionEventArgs args(position2D, 0, bodyIdx);
+        ofNotifyEvent(events.interactionEnded, args, this);
+    }
+    else if (prevState == k4w::HandState_Closed && currState == k4w::HandState_Open) {
+        CloudsInteractionEventArgs args(position2D, 2, bodyIdx);
+        ofNotifyEvent(events.interactionEnded, args, this);
+    }
+    
+    else if (currState == k4w::HandState_Lasso) {
+        CloudsInteractionEventArgs args(position2D, 0, bodyIdx);
+        ofNotifyEvent(events.interactionDragged, args, this);
+    }    
+    else if (currState == k4w::HandState_Closed) {
+        CloudsInteractionEventArgs args(position2D, 2, bodyIdx);
+        ofNotifyEvent(events.interactionDragged, args, this);
+    }
+    else if (currState == k4w::HandState_Open) {
+        CloudsInteractionEventArgs args(position2D, 0, bodyIdx);
+        ofNotifyEvent(events.interactionMoved, args, this);
+    }
+}
+
---
+++ CloudsLibrary/src/Input/CloudsInputKinectOSC.h
@@ -0,0 +1,118 @@
+//
+//  CloudsInputKinectOSC.h
+//  YellowTail
+//
+//  Created by Elie Zananiri on 12/8/13.
+//  
+//
+
+#pragma once
+
+#include "CloudsInput.h"
+
+#include "ofxOsc.h"
+
+namespace k4w 
+{
+    enum TrackingState 
+    {
+        TrackingState_NotTracked = 0,
+        TrackingState_Inferred   = 1,
+        TrackingState_Tracked    = 2
+    };
+    
+    enum HandState 
+    {
+        HandState_Unknown    = 0,
+        HandState_NotTracked = 1,
+        HandState_Open       = 2,
+        HandState_Closed     = 3,
+        HandState_Lasso      = 4
+    };
+    
+    enum JointType 
+    {
+        JointType_SpineBase     = 0,
+        JointType_SpineMid      = 1,
+        JointType_Neck          = 2,
+        JointType_Head          = 3,
+        JointType_ShoulderLeft	= 4,
+        JointType_ElbowLeft     = 5,
+        JointType_WristLeft     = 6,
+        JointType_HandLeft      = 7,
+        JointType_ShoulderRight	= 8,
+        JointType_ElbowRight	= 9,
+        JointType_WristRight	= 10,
+        JointType_HandRight	    = 11,
+        JointType_HipLeft       = 12,
+        JointType_KneeLeft      = 13,
+        JointType_AnkleLeft     = 14,
+        JointType_FootLeft      = 15,
+        JointType_HipRight      = 16,
+        JointType_KneeRight     = 17,
+        JointType_AnkleRight	= 18,
+        JointType_FootRight     = 19,
+        JointType_SpineShoulder	= 20,
+        JointType_HandTipLeft	= 21,
+        JointType_ThumbLeft     = 22,
+        JointType_HandTipRight	= 23,
+        JointType_ThumbRight	= 24,
+        
+        JointType_Count
+    };
+    
+    struct Joint 
+    {
+        JointType type;
+        TrackingState trackingState;
+        ofVec3f position;
+    };
+    
+    struct HandJoint : public Joint
+    {
+        HandState handState;
+    };
+    
+    class Body 
+    {
+    public:
+        Body(){
+            // set default joint attributes
+            headJoint.type = JointType_Head;
+            headJoint.trackingState = TrackingState_NotTracked;
+            
+            leftHandJoint.type = JointType_HandLeft;
+            leftHandJoint.trackingState = TrackingState_NotTracked;
+            leftHandJoint.handState = HandState_NotTracked;
+            
+            rightHandJoint.type = JointType_HandLeft;
+            rightHandJoint.trackingState = TrackingState_NotTracked;
+            rightHandJoint.handState = HandState_NotTracked;
+        }
+        
+        int idx;
+        
+        int lastUpdateFrame;
+        
+        Joint headJoint;
+        HandJoint leftHandJoint;
+        HandJoint rightHandJoint;
+    };
+};
+
+class CloudsInputKinectOSC : public CloudsInput
+{
+public:
+	
+	virtual void enable();
+	virtual void disable();
+    
+    void update(ofEventArgs& args);
+	
+	void processHandEvent(k4w::HandState prevState, k4w::HandState currState, int bodyIdx, ofVec3f& position);
+    
+    ofxOscReceiver receiver;
+    int lastOscFrame;
+    
+    map<int, k4w::Body *> bodies;
+};
