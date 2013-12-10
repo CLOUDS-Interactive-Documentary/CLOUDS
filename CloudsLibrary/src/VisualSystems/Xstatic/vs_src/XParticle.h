9a1fb2ab1f2ceddebd8ae7db9a9e5d665bd55e95
Elie Zananiri <prisonerjohn@gmail.com>
2013-12-10 16:01:00 -0500

    Xstatic: Added lots and lots of things.

--- CloudsLibrary/src/VisualSystems/Xstatic/vs_src/XParticle.h
+++ CloudsLibrary/src/VisualSystems/Xstatic/vs_src/XParticle.h
@@ -6,31 +6,24 @@
 //
 //
 
-#ifndef __Xstatic__particle__
-#define __Xstatic__particle__
+#pragma once
 
-#include <iostream>
 #include "ofMain.h"
-#include <set>
 
-class XParticle {
- 
+class XParticle
+{ 
 public:
+    XParticle();
     
-    int* minBri;
-    int* maxBri;
-    float* mass;
-    
-    XParticle(float& mass, float vx, float vy, float vz, int& minBri, int& maxBri);
-    
-    void applyForce(ofVec3f _force);
+    void applyForce(ofVec3f force);
     void update(float drag);
-    void display();
     
     void bounceEdges();
     void wrapEdges();
     
     void lottery();
+    
+    float mappedSize();
     
     ofVec3f location;
     ofVec3f velocity;
@@ -40,23 +33,34 @@ public:
     static float dt;
     static ofVec3f upperBounds;
     static ofVec3f lowerBounds;
+    static float diagonalSquared;
     
-    static float briDuration;
-    static float briRange;
+    static float minSize;
+    static float maxSize;
+    static float heightToSize;
+    static float velToSize;
+
+    static float minBri;
+    static float maxBri;
     
-    float uniqueVal;
-    int brightness;
+    static float oscSpeed;
     
+    static float triggerDuration;
+    static float triggerRange;
+    
+    static bool bOscillateSize;
+    static bool bOscillateBri;
+    
+    float size;
+    float colorPicker;
+    float brightness;
+    
+    float oscPos;
+        
 protected:
-    
-    float newBrightness;
-    
-    bool triggered;
+    bool bTriggered;
     int luckyNumber;
     int newRandomNumber;
     int previousTime = 0;
     int currentTime;
-        
 };
-
-#endif /* defined(__Xstatic__particle__) */
