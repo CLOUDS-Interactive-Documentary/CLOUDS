23e9402b9d06980f91150379d6902012bf3aa5a4
Neil Mendoza <neil@isthis.gd>
2013-12-12 09:27:48 +0000

    Rulez: resize post

--- CloudsLibrary/src/VisualSystems/Rulez/vs_src/CloudsVisualSystemRulez.h
+++ CloudsLibrary/src/VisualSystems/Rulez/vs_src/CloudsVisualSystemRulez.h
@@ -105,9 +105,9 @@ class CloudsVisualSystemRulez : public CloudsVisualSystem {
 	}
 
 protected:
-    
     //  Your Stuff
     //
+    void onWindowResized(ofResizeEventArgs& args);
     void generate();
     void saveMesh();
     
