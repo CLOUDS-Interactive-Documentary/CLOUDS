b8aa0ade7c7573929e9d3fd698a39cbf32011e50
James George <james@jamesgeorge.org>
2013-12-14 23:41:48 -0500

    fixed state leak for Higa's old system

---
+++ CloudsLibrary/src/RGBD/HUD/CloudsHUDLayer.cpp
@@ -0,0 +1,9 @@
+//
+//  CloudsHUDLayer.cpp
+//  CloudsHUDDesigner
+//
+//  Created by James George on 12/14/13.
+//
+//
+
+#include "CloudsHUDLayer.h"
---
+++ CloudsLibrary/src/RGBD/HUD/CloudsHUDLayer.h
@@ -0,0 +1,14 @@
+//
+//  CloudsHUDLayer.h
+//  CloudsHUDDesigner
+//
+//  Created by James George on 12/14/13.
+//
+//
+
+#ifndef __CloudsHUDDesigner__CloudsHUDLayer__
+#define __CloudsHUDDesigner__CloudsHUDLayer__
+
+#include <iostream>
+
+#endif /* defined(__CloudsHUDDesigner__CloudsHUDLayer__) */
