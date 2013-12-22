68068db4d16c6d0550a7df9382381c76d34edd04
lars berg <laserstorm@gmail.com>
2013-12-22 12:17:43 -0600

    Astrolabe: added gui controls still need to ad a few more states. just wanted to get the gui working before I started expanding

--- CloudsLibrary/src/VisualSystems/Astrolabe/vs_src/Astrolabe.cpp
+++ CloudsLibrary/src/VisualSystems/Astrolabe/vs_src/Astrolabe.cpp
@@ -37,16 +37,18 @@ void Astrolabe::setupMesh( float _lowRadian, float _hiRadian, float _innerRadius
 	
 	
 	//set the profile curve
-	int subDivisions = abs(r1 - r0) / .1;
+	int subDivisions = abs(r1 - r0) / .05;
 	
-	profile.clear();
-	for( int i=0; i<subDivisions; i++)
+	
+	profile.resize(subDivisions);
+	
+	cout << "profile.size(): " << profile.size() << endl;
+	for( int i=0; i<profile.size(); i++)
 	{
-		float radian = ofMap(i, 0, subDivisions-1, r0, r1, true);
-		profile.push_back( ofVec3f( sin(radian), 0, cos(radian) ) );
+		float radian = ofMap(i, 0, profile.size()-1, r0, r1, true);
+		profile[i].set( sin(radian), 0, cos(radian) );
 	}
-	
-	
+
 	buildTheMesh();
 };
 
@@ -104,90 +106,92 @@ ofVec3f Astrolabe::normalFromThreePoints(ofVec3f p0, ofVec3f p1, ofVec3f p2)
 
 void Astrolabe::buildTheMesh()
 {
-	
-	//create our mesh
-	numVertices = 16 * profile.size() + 8;//top, bottom, front, back, lofts & caps
-	vertices.resize( numVertices );
-	normals.resize( numVertices );
-	
-	//add the vertices
-	ofVec3f halfThick(0,thickness * .5 , 0);
-	
-	for(int i=0; i<profile.size()-1; i++)
+	if(profile.size() > 1)
 	{
-		ofVec3f v0 = profile[i] * innerRadius;
-		ofVec3f v1 = profile[i] * outerRadius;
-		ofVec3f v2 = profile[i+1] * outerRadius;
-		ofVec3f v3 = profile[i+1] * innerRadius;
+		//create our mesh
+		numVertices = 16 * profile.size() + 8;//top, bottom, front, back, lofts & caps
+		vertices.resize( numVertices );
+		normals.resize( numVertices );
 		
-		vertices[i*16 + 0] =  v0 + halfThick;
-		vertices[i*16 + 1] =  v1 + halfThick;
-		vertices[i*16 + 2] =  v2 + halfThick;
-		vertices[i*16 + 3] =  v3 + halfThick;
+		//add the vertices
+		ofVec3f halfThick(0,thickness * .5 , 0);
 		
-		vertices[i*16 + 4] = v0 - halfThick;
-		vertices[i*16 + 5] = v3 - halfThick;
-		vertices[i*16 + 6] = v2 - halfThick;
-		vertices[i*16 + 7] = v1 - halfThick;
+		for(int i=0; i<profile.size()-1; i++)
+		{
+			ofVec3f v0 = profile[i] * innerRadius;
+			ofVec3f v1 = profile[i] * outerRadius;
+			ofVec3f v2 = profile[i+1] * outerRadius;
+			ofVec3f v3 = profile[i+1] * innerRadius;
+			
+			vertices[i*16 + 0] =  v0 + halfThick;
+			vertices[i*16 + 1] =  v1 + halfThick;
+			vertices[i*16 + 2] =  v2 + halfThick;
+			vertices[i*16 + 3] =  v3 + halfThick;
+			
+			vertices[i*16 + 4] = v0 - halfThick;
+			vertices[i*16 + 5] = v3 - halfThick;
+			vertices[i*16 + 6] = v2 - halfThick;
+			vertices[i*16 + 7] = v1 - halfThick;
+			
+			vertices[i*16 + 8] = v1 + halfThick;
+			vertices[i*16 + 9] = v1 - halfThick;
+			vertices[i*16 + 10] = v2 - halfThick;
+			vertices[i*16 + 11] = v2 + halfThick;
+			
+			vertices[i*16 + 12] = v3 + halfThick;
+			vertices[i*16 + 13] = v3 - halfThick;
+			vertices[i*16 + 14] = v0 - halfThick;
+			vertices[i*16 + 15] = v0 + halfThick;
+			
+			
+			//normals
+			fill (normals.begin()+(i*16), normals.begin()+(i*16+3), ofVec3f(0,1,0));
+			fill (normals.begin()+(i*16+4), normals.begin()+(i*16+7), ofVec3f(0,-1,0));
+			
+			normals[i*16 + 8] = profile[i];
+			normals[i*16 + 9] = profile[i];
+			normals[i*16 + 10] = profile[i+1];
+			normals[i*16 + 11] = profile[i+1];
+			
+			normals[i*16 + 12] = -profile[i+1];
+			normals[i*16 + 13] = -profile[i+1];
+			normals[i*16 + 14] = -profile[i];
+			normals[i*16 + 15] = -profile[i];
+			
+		}
 		
-		vertices[i*16 + 8] = v1 + halfThick;
-		vertices[i*16 + 9] = v1 - halfThick;
-		vertices[i*16 + 10] = v2 - halfThick;
-		vertices[i*16 + 11] = v2 + halfThick;
+		//cap the ends
+		//front
+		ofVec3f v0 = profile.front() * innerRadius + halfThick;
+		ofVec3f v1 = profile.front() * outerRadius + halfThick;
+		ofVec3f v2 = profile.front() * outerRadius - halfThick;
+		ofVec3f v3 = profile.front() * innerRadius - halfThick;
 		
-		vertices[i*16 + 12] = v3 + halfThick;
-		vertices[i*16 + 13] = v3 - halfThick;
-		vertices[i*16 + 14] = v0 - halfThick;
-		vertices[i*16 + 15] = v0 + halfThick;
+		*(vertices.end()-8) = v0;
+		*(vertices.end()-7) = v3;
+		*(vertices.end()-6) = v2;
+		*(vertices.end()-5) = v1;
 		
+		fill (normals.end()-8, normals.end()-5, normalFromThreePoints(v0,v2,v1) );
 		
-		//normals
-		fill (normals.begin()+(i*16), normals.begin()+(i*16+3), ofVec3f(0,1,0));
-		fill (normals.begin()+(i*16+4), normals.begin()+(i*16+7), ofVec3f(0,-1,0));
+		//back
+		v0 = profile.back() * innerRadius + halfThick;
+		v1 = profile.back() * outerRadius + halfThick;
+		v2 = profile.back() * outerRadius - halfThick;
+		v3 = profile.back() * innerRadius - halfThick;
 		
-		normals[i*16 + 8] = profile[i];
-		normals[i*16 + 9] = profile[i];
-		normals[i*16 + 10] = profile[i+1];
-		normals[i*16 + 11] = profile[i+1];
+		*(vertices.end()-4) = v0;
+		*(vertices.end()-3) = v1;
+		*(vertices.end()-2) = v2;
+		*(vertices.end()-1) = v3;
 		
-		normals[i*16 + 12] = -profile[i+1];
-		normals[i*16 + 13] = -profile[i+1];
-		normals[i*16 + 14] = -profile[i];
-		normals[i*16 + 15] = -profile[i];
+		fill (normals.end()-4, normals.end()-1, normalFromThreePoints(v0,v1,v2) );
 		
+		//build the vbo
+		vbo.clear();
+		vbo.setVertexData( &vertices[0], vertices.size(), GL_DYNAMIC_DRAW );
+		vbo.setNormalData( &normals[0], normals.size(), GL_DYNAMIC_DRAW );
 	}
-	
-	//cap the ends
-	//front
-	ofVec3f v0 = profile.front() * innerRadius + halfThick;
-	ofVec3f v1 = profile.front() * outerRadius + halfThick;
-	ofVec3f v2 = profile.front() * outerRadius - halfThick;
-	ofVec3f v3 = profile.front() * innerRadius - halfThick;
-	
-	*(vertices.end()-8) = v0;
-	*(vertices.end()-7) = v3;
-	*(vertices.end()-6) = v2;
-	*(vertices.end()-5) = v1;
-	
-	fill (normals.end()-8, normals.end()-5, normalFromThreePoints(v0,v2,v1) );
-	
-	//back
-	v0 = profile.back() * innerRadius + halfThick;
-	v1 = profile.back() * outerRadius + halfThick;
-	v2 = profile.back() * outerRadius - halfThick;
-	v3 = profile.back() * innerRadius - halfThick;
-	
-	*(vertices.end()-4) = v0;
-	*(vertices.end()-3) = v1;
-	*(vertices.end()-2) = v2;
-	*(vertices.end()-1) = v3;
-	
-	fill (normals.end()-4, normals.end()-1, normalFromThreePoints(v0,v1,v2) );
-	
-	//build the vbo
-	vbo.clear();
-	vbo.setVertexData( &vertices[0], vertices.size(), GL_DYNAMIC_DRAW );
-	vbo.setNormalData( &normals[0], normals.size(), GL_DYNAMIC_DRAW );
 }
 
 void Astrolabe::setVbo( ofVbo& _vbo, int _numVertices )
