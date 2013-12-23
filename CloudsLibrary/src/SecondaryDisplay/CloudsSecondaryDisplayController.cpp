7d56a18d24531f88f47718b6528aff5601f136b0
James George <james@jamesgeorge.org>
2013-12-23 16:40:36 -0500

    rename cluster map

---
+++ CloudsLibrary/src/SecondaryDisplay/CloudsSecondaryDisplayController.cpp
@@ -0,0 +1,9 @@
+//
+//  CloudsSecondaryDisplayController.cpp
+//  CloudsSecondaryDisplay
+//
+//  Created by James George on 12/23/13.
+//
+//
+
+#include "CloudsSecondaryDisplayController.h"
---
+++ CloudsLibrary/src/SecondaryDisplay/CloudsSecondaryDisplayController.h
@@ -0,0 +1,14 @@
+//
+//  CloudsSecondaryDisplayController.h
+//  CloudsSecondaryDisplay
+//
+//  Created by James George on 12/23/13.
+//
+//
+
+#ifndef __CloudsSecondaryDisplay__CloudsSecondaryDisplayController__
+#define __CloudsSecondaryDisplay__CloudsSecondaryDisplayController__
+
+#include <iostream>
+
+#endif /* defined(__CloudsSecondaryDisplay__CloudsSecondaryDisplayController__) */
