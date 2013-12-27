af70a034f53c800a35c79b4646b26c91896740f2
Surya Mattu <surya@suryamattu.com>
2013-12-27 15:29:42 -0500

    using selfInteractionMoved instead of mouse moved added maps to track individual players

--- CloudsLibrary/src/VisualSystems/GesturePaint/vs_src/CloudsVisualSystemGesturePaint.h
+++ CloudsLibrary/src/VisualSystems/GesturePaint/vs_src/CloudsVisualSystemGesturePaint.h
@@ -14,7 +14,7 @@
 
 //TODO: rename this to your own visual system
 class CloudsVisualSystemGesturePaint : public CloudsVisualSystem {
-  public:
+public:
     
 	//TODO: Change this to the name of your visual system
 	//This determines your data path so name it at first!
@@ -22,7 +22,7 @@ class CloudsVisualSystemGesturePaint : public CloudsVisualSystem {
     string getSystemName(){
 		return "GesturePaint";
 	}
-
+    
 	//These methods let us add custom GUI parameters and respond to their events
     void selfSetupGui();
     void selfGuiEvent(ofxUIEventArgs &e);
@@ -34,7 +34,7 @@ class CloudsVisualSystemGesturePaint : public CloudsVisualSystem {
 	//use render gui for display settings, like changing colors
     void selfSetupRenderGui();
     void guiRenderEvent(ofxUIEventArgs &e);
-
+    
 	
 	void selfSetDefaults();
 	
@@ -42,12 +42,12 @@ class CloudsVisualSystemGesturePaint : public CloudsVisualSystem {
 	// This will be called during a "loading" screen, so any big images or
 	// geometry should be loaded here
     void selfSetup();
-
+    
 	// selfBegin is called when the system is ready to be shown
 	// this is a good time to prepare for transitions
 	// but try to keep it light weight as to not cause stuttering
     void selfBegin();
-
+    
 	// selfPresetLoaded is called whenever a new preset is triggered
 	// it'll be called right before selfBegin() and you may wish to
 	// refresh anything that a preset may offset, such as stored colors or particles
@@ -59,46 +59,44 @@ class CloudsVisualSystemGesturePaint : public CloudsVisualSystem {
 	
 	//normal update call
 	void selfUpdate();
-
+    
 	// selfDraw draws in 3D using the default ofEasyCamera
 	// you can change the camera by returning getCameraRef()
     void selfDraw();
-		
+    
     // draw any debug stuff here
 	void selfDrawDebug();
-
+    
 	// or you can use selfDrawBackground to do 2D drawings that don't use the 3D camera
 	void selfDrawBackground();
-
+    
 	// this is called when your system is no longer drawing.
 	// Right after this selfUpdate() and selfDraw() won't be called any more
 	void selfEnd();
-
+    
 	// this is called when you should clear all the memory and delet anything you made in setup
     void selfExit();
-
+    
 	//events are called when the system is active
 	//Feel free to make things interactive for you, and for the user!
     void selfKeyPressed(ofKeyEventArgs & args);
     void selfKeyReleased(ofKeyEventArgs & args);
     
-    void selfMouseDragged(ofMouseEventArgs& data);
-    void selfMouseMoved(ofMouseEventArgs& data);
-    void selfMousePressed(ofMouseEventArgs& data);
-    void selfMouseReleased(ofMouseEventArgs& data);
+    void selfInteractionMoved(CloudsInteractionEventArgs& args);
+	void selfInteractionStarted(CloudsInteractionEventArgs& args);
+	void selfInteractionDragged(CloudsInteractionEventArgs& args);
+	void selfInteractionEnded(CloudsInteractionEventArgs& args);
 	
-
+    
 protected:
     
-    //  Your Stuff
-    //
 	
 	ofxUISuperCanvas* waterGui;
 	float blurRadius;
 	float dryRate;
 	float depositeScale;
 	float currentDepositeScale;
-
+    
 	bool showWaterDebug;
 	void createWaterBrush();
 	
@@ -121,7 +119,7 @@ protected:
 	ofShader forceBrushShader;
 	ofShader paintBrushShader;
 	ofShader paperMixShader;
-
+    
 	ofVec2f palettePosition;
 	ofImage palette;
 	ofImage noiseFlowTex;
@@ -135,6 +133,8 @@ protected:
 	ofRectangle paperRect;
 	vector<ofVec2f> depositPoints;
 	vector<ofVec2f> mouseHistory;
+    map<int, vector<ofVec2f> > playerHistoryMap;
+    map<int, vector<ofVec2f> > playerDepositPoints;
 	
 	void reallocateFramebuffers();
 	void meshFromFbo(ofMesh& m, ofFbo& f);
