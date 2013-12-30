0e72b543a1eefd724661f28077e0fbc30ceddd0b
Omer Shapira <info@omershapira.com>
2013-12-29 21:27:34 -0500

    [Colony] optics distortion in cell

--- CloudsData/visualsystems/Colony/shaders/levelSet.fs
+++ CloudsData/visualsystems/Colony/shaders/levelSet.fs
@@ -1,6 +1,6 @@
 #version 120
 //#extension GL_ARB_shader_texture_lod: enable
-#extension GL_ARB_texture_rectangle: enable
+//#extension GL_ARB_texture_rectangle: enable
 #define PI 3.14159265359
 #define E 2.71828
 
@@ -16,13 +16,13 @@ uniform vec4 kernelColor_high;
 uniform vec4 kernelColor_low;
 uniform float kernel_maxValue;
 uniform vec3 lightDirection;
-uniform vec4 lightColor;
+uniform vec3 lightColor;
 
-/*
+
 float rand(vec2 co){
     return fract(sin(dot(co.xy, vec2(12.9898, 78.233))) * 43758.5453);
 }
-
+/*
 //Hash-Based Noise Func
 float noise2f( in vec2 p ){
     vec2 ip = vec2(floor(p));
@@ -79,37 +79,45 @@ float getLightIntensity(float elevation, vec2 light){
 }
 
 
+
+
 vec4 getLevelSet(vec4 fg){
     float a = PI * (.5 + log(.25 + fg.b) * 6.);
     float b = heightMap(gl_TexCoord[0].xy) * 18.;
     float g = fg.g + .1;
     g *= g * g;
     float levl = mix (a + b, max(a, b), .5);
-    float set = (.5 * (1. + sin(levl)) + g) 
-                * mix(1, getLightIntensity(levl, lightDirection.xy), 0.3);
-	return vec4(set, set, set, 1.);
+    float set = (.5 * (1. + sin(levl)) + g);
+    float light = getLightIntensity(levl, lightDirection.xy);
+	return vec4(set
+                * mix(1, light, 0.3)
+                * mix(vec3(1.),lightColor, light),
+                1.);
 }
 
-vec4 getMicroscope(vec4 fg, vec4 bg){
+
+
+vec4 getMicroscope(vec4 fg){
     //Check if you're on an area of change
     float b = 1. - fwidth(fg.b);
     
     //see if you're in the right range to be a border
     b *= fg.b * fg.b;
-    float innerCellAlpha = clamp(bump(b, .8, .4), 0., translucenseCell);
-    //innerCell *= (1. - (.2 + .2 * sin(gl_FragCoord.x + gl_FragCoord.y)));
+    float innerCellAlpha = clamp(bump(b, .8, .8), 0., translucenseCell);
+//    innerCellAlpha *= (1. - (.2 + .2 * sin(gl_FragCoord.x + gl_FragCoord.y)));
     float shellAlpha = clamp(bump(b, .3, .2), 0., .95);
     vec4 kernel = clamp(fg.g/kernel_maxValue, 0., 1.)
 //                * vec4(vec3(mix(1.,getLightIntensity(fg.g/kernel_maxValue, vec3(1)), 0.3)),1.)
                 * mix(kernelColor_low, kernelColor_high, fg.g/kernel_maxValue);
-    vec4 shell = pow(shellAlpha, 1.5)
-               * vec4(1.);
+    vec4 shell = vec4(1.,1.,1.,pow(shellAlpha, 1.5));
+    
+    vec2 normalizedCoords = gl_TexCoord[0].xy * imgRes / resolution;
+    vec4 bg = texture2DRect(grunge, normalizedCoords * .5  + vec2(dFdx(fg.b),dFdy(fg.b)) * 100.0+ imgRes * .25 ); //enlarged
     
     //COMPOSITING STAGE
     vec4 ret = over(premult(shell), premult(kernel)); //top
     ret = over(premult(ret), premult(vec4(bg.rgb, bg.a*innerCellAlpha)));
     return ret;
-    
 }
 
 void main(){
@@ -124,10 +132,11 @@ void main(){
         vec2 normalizedCoords = gl_TexCoord[0].xy * imgRes / resolution;
         vec4 bg = texture2DRect(grunge, normalizedCoords);
         vec4 bg_cu = texture2DRect(grunge, normalizedCoords * .5 + imgRes * .25 ); //enlarged
-        vec4 cells = getMicroscope(fg, bg_cu);
+        vec4 cells = getMicroscope(fg);
         bg.a *= translucenseDish;
         color = over(premult(cells), premult(bg));
     }
+    
     gl_FragColor = color;
 }
 
