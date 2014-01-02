b45e1babdcf1a56dbc7c36cb380c22d018e440b2
Jack Kalish <jack@jackkalish.com>
2014-01-01 21:13:53 -0500

    fix for osc port numbers working on integrating animations

--- CloudsLibrary/src/SecondaryDisplay/Receiver/CloudsSecondaryDisplayController.cpp
+++ CloudsLibrary/src/SecondaryDisplay/Receiver/CloudsSecondaryDisplayController.cpp
@@ -16,21 +16,22 @@ CloudsSecondaryDisplayController::CloudsSecondaryDisplayController(){
     lightBlue = ofColor::fromHex(0x97d7fb);
     darkBlue = ofColor::fromHex(0x439ced);
     color = false;
+    stringCounter = 0;
 }
 
 void CloudsSecondaryDisplayController::setup(){
-
+    
 	parser.loadFromFiles();
 	
 	clusterMap.buildEntireCluster(parser);
-
+    
 	clusterMap.forceScreenResolution(1920, 1080);
 	clusterMap.setDrawToScreen(false);
 	
 	clusterMap.setup();
 	clusterMap.playSystem();
-
-	receiver.setup(12346);
+    
+	receiver.setup(123456);
 	
     
     
@@ -64,7 +65,7 @@ void CloudsSecondaryDisplayController::setup(){
     meshProjectTitle = projectLayout.getMeshByID("TEXTBOX_x5F_TITLE_1_");
     meshProjectArtist = projectLayout.getMeshByID("TEXTBOX_x5F_ARTIST_1_");
     meshProjectDescription = projectLayout.getMeshByID("TEXTBOX_x5F_DESC_1_");
-
+    
     //load all fonts
     ////last name
     h1 = getLayoutForLayer(meshBioLastName, "Blender-BOOK");
@@ -76,7 +77,7 @@ void CloudsSecondaryDisplayController::setup(){
     h4 = getLayoutForLayer(meshBioLocation, "Blender-BOOK");
     ////byline / description
     p = getLayoutForLayer(meshBioDescription, "Blender-BOOK");
-
+    
 	displayTarget.allocate(1920, 1080, GL_RGB);
     // cleanup!
     for( int i=0; i<tempFontListThin.size(); i++ ){
@@ -85,39 +86,44 @@ void CloudsSecondaryDisplayController::setup(){
     }
     tempFontListThin.clear();
     tempFontListBook.clear();
-
+    
 }
 
 /*LOADING SVG LAYOUT files from Sarah*/
 void CloudsSecondaryDisplayController::loadSVGs(){
 	/*ofDirectory svgs(GetCloudsDataPath() + "secondaryDisplay/SVG/BIO/");
-	svgs.allowExt("svg");
-	svgs.listDir();
-    //loading all the SVG files in the BIO dir, but why?
-	for(int i = 0; i < svgs.numFiles(); i++){
-		testAllLayout.push_back(CloudsSVGMesh());
-		testAllLayout.back().load(svgs.getPath(i));
-	}*/
+     svgs.allowExt("svg");
+     svgs.listDir();
+     //loading all the SVG files in the BIO dir, but why?
+     for(int i = 0; i < svgs.numFiles(); i++){
+     testAllLayout.push_back(CloudsSVGMesh());
+     testAllLayout.back().load(svgs.getPath(i));
+     }*/
     
     //load the three different layouts
     bioLayout.load(GetCloudsDataPath() + "secondaryDisplay/SVG/BIO/BIO.svg");
     projectLayout.load(GetCloudsDataPath() + "secondaryDisplay/SVG/PROJECTEX/PROJECTEX.svg");
     questionLayout.load(GetCloudsDataPath() + "secondaryDisplay/SVG/QUESTION/QUESTION.svg");
-
+    
 }
 
 ofxFTGLSimpleLayout* CloudsSecondaryDisplayController::getLayoutForLayer( SVGMesh* textMesh, string font) {
     
-        if( textMesh != NULL ){
-            
-            int fontSize = getFontSizeForMesh( textMesh, font );
-            // make a layout
-            ofxFTGLSimpleLayout *newLayout = new ofxFTGLSimpleLayout();
-            newLayout->loadFont( GetCloudsDataPath() + "font/"+font+".ttf", fontSize );
-            newLayout->setLineLength( textMesh->bounds.width );
-            
-            return newLayout;
-        }
+    if( textMesh != NULL ){
+        
+        int fontSize = getFontSizeForMesh( textMesh, font );
+        // make a layout
+        ofxFTGLSimpleLayout *newLayout = new ofxFTGLSimpleLayout();
+        newLayout->loadFont( GetCloudsDataPath() + "font/"+font+".ttf", fontSize );
+        newLayout->setLineLength( textMesh->bounds.width );
+        
+        // make a label
+        CloudsHUDLabel *newLabel = new CloudsHUDLabel();
+        newLabel->setup( newLayout, textMesh->bounds );
+        hudLabelMap[textMesh->id] = newLabel;
+        
+        return newLayout;
+    }
     
     return NULL;
 }
@@ -156,21 +162,31 @@ void CloudsSecondaryDisplayController::update(){
 	//TODO: fix with perma preset
 	//clusterMap.incrementalTraversalMode = true;
 	
+    stringCounter++;
+    
 	while(receiver.hasWaitingMessages()){
 		ofxOscMessage m;
 		receiver.getNextMessage(&m);
 		
 		//all the args sent for clip for reference
-//0		m.addStringArg(clip.person);//final cut person id
-//1		m.addStringArg(clip.getID());//clip id
-//2		m.addFloatArg(clip.getDuration());//duraiton
-//3		m.addStringArg(currentTopic); //topic
-//4		m.addStringArg(clip.projectExampleTitle); //example
-//5		m.addStringArg(lastQuestionAsked); //question
-
+        //0		m.addStringArg(clip.person);//final cut person id
+        //1		m.addStringArg(clip.getID());//clip id
+        //2		m.addFloatArg(clip.getDuration());//duraiton
+        //3		m.addStringArg(currentTopic); //topic
+        //4		m.addStringArg(clip.projectExampleTitle); //example
+        //5		m.addStringArg(lastQuestionAsked); //question
+        
 		if(m.getAddress() == "/clip"){
-			hasSpeaker = true;
+            stringCounter = 0;
+            
 			currentSpeaker = CloudsSpeaker::speakers[m.getArgAsString(0)];
+            
+            //if the speaker has no name, there is no speaker
+            if(currentSpeaker.lastName == "")
+                hasSpeaker = false;
+            else
+                hasSpeaker = true;
+            
 			currentClip = parser.getClipWithID(m.getArgAsString(1));
 			
 			clusterMap.traverseToClip(currentClip);
@@ -195,7 +211,13 @@ void CloudsSecondaryDisplayController::update(){
                 
                 //setup all bio data
                 lastQuestion = m.getArgAsString(5);
-                cout << "lastQuestion: '" << lastQuestion << "'";
+               // cout << "lastQuestion: '" << lastQuestion << "'";
+                
+              //  hudLabelMap["BylineFirstNameTextBox_1_"]->setText( currentSpeaker.firstName );
+              //  hudLabelMap["BylineLastNameTextBox"]->setText( currentSpeaker.lastName );
+               // hudLabelMap["BylineTopicTextBoxTop"]->setText( currentSpeaker.title );
+                //hudLabelMap["BylineTopicTextBoxBottom"]->setText( currentSpeaker.location2 );
+                //hudLabelMap["BylineBodyCopyTextBox"]->setText( currentSpeaker.byline1 );
 			}
 		}
         else if(m.getAddress() == "/actBegan"){
@@ -227,85 +249,84 @@ void CloudsSecondaryDisplayController::draw(){
 	displayTarget.begin();
 	
 	clusterMap.selfPostDraw();
-
+    
     SVGMesh* t;
     
     if(displayMode == "BIO"){
-        //DRAW BIO LAYOUT, need to draw this first, text goes over it
-        bioLayout.draw();
-        
         ////question
         ////// don't display if not avilable
-        //find the question box
         if(lastQuestion != ""){
             //show the question box
             questionLayout.draw();
             //find the text box
             lastQuestion = ofToUpper(lastQuestion);
             drawTextToMesh(h3, lastQuestion, meshQuestion);
-         //   if(debug)
-           //     ofRect(t->bounds);
         }
         
-        ////speaker name
-        string firstName, lastName;
+        //on;y draw speaker info if there is a speaker, duh
         if(hasSpeaker){
-            firstName = currentSpeaker.firstName;
-            lastName = currentSpeaker.lastName;
+            //DRAW BIO LAYOUT, need to draw this first, text goes over it
+            bioLayout.draw();
+            
+            ////speaker name
+            string firstName, lastName;
+            if(hasSpeaker){
+                firstName = currentSpeaker.firstName;
+                lastName = currentSpeaker.lastName;
+            }
+            else{
+                firstName = "NO";
+                lastName = "SPEAKER";
+            }
+            
+            //DRAW SPEAKER NAME
+            ////first name
+            firstName = ofToUpper(firstName);
+            drawTextToMesh(h2, firstName, meshBioFirstName);
+            
+            ////last name
+            lastName = ofToUpper(lastName);
+            drawTextToMesh(h1, lastName, meshBioLastName);
+            
+            float firstNameWidth = h2->getStringBoundingBox(firstName, 0, 0).width;
+            float lastNameWidth = h1->getStringBoundingBox(lastName, 0, 0).width;
+            float longestNameWidth;
+            if(firstNameWidth > lastNameWidth)
+                longestNameWidth = firstNameWidth;
+            else
+                longestNameWidth = lastNameWidth;
+            
+            float margin = 60;
+            float titleX = meshBioFirstName->bounds.x + longestNameWidth + margin;
+            
+            ////title
+            string title = ofToUpper(currentSpeaker.title);
+            meshBioTitle->bounds.width = 9999;
+            //reposition title to float left
+            meshBioTitle->bounds.x = titleX;
+            
+            if(color)
+                ofSetColor(lightBlue);
+            drawTextToMesh(h4, title, meshBioTitle);
+            if(debug)
+                //ofRect(t->bounds);
+                
+                ////location
+                if(color)
+                    ofSetColor(darkBlue);
+            
+            string loc = ofToUpper(currentSpeaker.location2);
+            meshBioLocation->bounds.x = titleX;
+            meshBioLocation->bounds.width = 9999;
+            drawTextToMesh(h4, loc, meshBioLocation);
+            
+            if(color)
+                ofSetColor(255);
+            
+            ////byline / bio / description
+            drawTextToMesh(p, currentSpeaker.byline1, meshBioDescription);
+            
         }
-        else{
-            firstName = "NO";
-            lastName = "SPEAKER";
-        }
-        
-        //DRAW SPEAKER NAME
-        ////first name
-        firstName = ofToUpper(firstName);
-        drawTextToMesh(h2, firstName, meshBioFirstName);
-        
-        ////last name
-        lastName = ofToUpper(lastName);
-        drawTextToMesh(h1, lastName, meshBioLastName);
-        
-        float firstNameWidth = h2->getStringBoundingBox(firstName, 0, 0).width;
-        float lastNameWidth = h1->getStringBoundingBox(lastName, 0, 0).width;
-        float longestNameWidth;
-        if(firstNameWidth > lastNameWidth)
-            longestNameWidth = firstNameWidth;
-        else
-            longestNameWidth = lastNameWidth;
-        
-        float margin = 60;
-        float titleX = meshBioFirstName->bounds.x + longestNameWidth + margin;
-        
-        ////title
-        string title = ofToUpper(currentSpeaker.title);
-        meshBioTitle->bounds.width = 9999;
-        //reposition title to float left
-        meshBioTitle->bounds.x = titleX;
-        
-        if(color)
-            ofSetColor(lightBlue);
-        drawTextToMesh(h4, title, meshBioTitle);
-        if(debug)
-            //ofRect(t->bounds);
-        
-        ////location
-        if(color)
-            ofSetColor(darkBlue);
-
-        string loc = ofToUpper(currentSpeaker.location2);
-        meshBioLocation->bounds.x = titleX;
-        meshBioLocation->bounds.width = 9999;
-        drawTextToMesh(h4, loc, meshBioLocation);
-        
-        if(color)
-            ofSetColor(255);
-        
-        ////byline / bio / description
-        drawTextToMesh(p, currentSpeaker.byline1, meshBioDescription);
-
-        
         
         
     }else if(displayMode == "PROJECT"){
@@ -322,15 +343,15 @@ void CloudsSecondaryDisplayController::draw(){
         }
         
         ////project title
-            string title = ofToUpper(currentExample.title);
-            drawTextToMesh(h2, title, meshProjectTitle);
+        string title = ofToUpper(currentExample.title);
+        drawTextToMesh(h2, title, meshProjectTitle);
         
         ////artist name
-            string name = currentExample.creatorName;
-            drawTextToMesh(h4, name, meshProjectArtist);
+        string name = currentExample.creatorName;
+        drawTextToMesh(h4, name, meshProjectArtist);
         
         ////project description
-            drawTextToMesh(p, currentExample.description, meshProjectDescription);
+        drawTextToMesh(p, currentExample.description, meshProjectDescription);
         
     }
 	
@@ -345,13 +366,13 @@ void CloudsSecondaryDisplayController::draw(){
 }
 
 /*void CloudsSecondaryDisplayController::drawBioLayout(){
-    
-}*/
+ 
+ }*/
 
 void CloudsSecondaryDisplayController::drawTextToMesh(ofxFTGLSimpleLayout* font, string text, SVGMesh* mesh){
     //update line length
     font->setLineLength(mesh->bounds.width);
     //draw the text
-    font->drawString(text, mesh->bounds.x, mesh->bounds.y + font->getStringBoundingBox("M", 0, 0).height);
+    font->drawString(text.substr(0), mesh->bounds.x, mesh->bounds.y + font->getStringBoundingBox("M", 0, 0).height);
 }
 
