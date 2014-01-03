e12e8f396ca77e139442a779562bf97a6d3c0f03
Neil Mendoza <neil@isthis.gd>
2014-01-03 15:49:45 +0000

    Swim: tentacles WIP

--- CloudsLibrary/src/VisualSystems/Swim/vs_src/Creatures.cpp
+++ CloudsLibrary/src/VisualSystems/Swim/vs_src/Creatures.cpp
@@ -101,7 +101,6 @@ namespace itg
         tentacles.getDrawShaderRef().load(dataPath + "shaders/tentaclesDraw");
         tentacles.setTextureLocation(1);
         
-        //generate();
         pointCreatureMesh.setUsage(GL_DYNAMIC_DRAW);
         pointCreatureMesh.setMode(OF_PRIMITIVE_POINTS);
     }
@@ -137,11 +136,11 @@ namespace itg
         // tentacles
         unsigned numTentacles = 0;
         for (unsigned i = 0; i < jellies.size(); ++i) numTentacles += jellies[i]->getNumTentacles();
-        tentacles.init(TENTACLE_NUM_SECTIONS, numTentacles, OF_PRIMITIVE_POINTS, false);
+        tentacles.init(TENTACLE_NUM_SECTIONS, numTentacles, OF_PRIMITIVE_LINES, false);
         if (numTentacles * TENTACLE_NUM_SECTIONS * 4 != tentacles.getSize()) ofLogFatalError() << "tentacle texture size error";
         float* particlePosns = new float[tentacles.getSize()];
-        
         unsigned tentacleIdx = 0;
+        ofVboMesh& tentacleMesh = tentacles.getMeshRef();
         for (unsigned i = 0; i < jellies.size(); ++i)
         {
             vector<ofVec3f> deformed = jellies[i]->getDeformedTentaclePosns();
@@ -154,24 +153,15 @@ namespace itg
                     particlePosns[idx * 4 + 1] = deformed[j].y - x * TENTACLE_SECTION_LENGTH;
                     particlePosns[idx * 4 + 2] = deformed[j].z;
                     particlePosns[idx * 4 + 3] = 0.f;
+                    if (x > 0)
+                    {
+                        tentacleMesh.addIndex(TENTACLE_NUM_SECTIONS * tentacleIdx + x - 1);
+                        tentacleMesh.addIndex(TENTACLE_NUM_SECTIONS * tentacleIdx + x);
+                    }
                 }
                 ++tentacleIdx;
             }
         }
-        
-        /*
-        for (unsigned y = 0; y < jellies.size(); ++y)
-        {
-            ofVec3f pos = jellies[y]->getPosition();
-            for (unsigned x = 0; x < TENTACLE_NUM_SECTIONS; ++x)
-            {
-                unsigned idx = y * TENTACLE_NUM_SECTIONS + x;
-                particlePosns[idx * 4] = pos.x;
-                particlePosns[idx * 4 + 1] = pos.y - x * TENTACLE_SECTION_LENGTH;
-                particlePosns[idx * 4 + 2] = pos.z;
-                particlePosns[idx * 4 + 3] = 0.f;
-            }
-        }*/
         tentacles.loadDataTexture(ofxGpuParticles::POSITION, particlePosns);
         delete[] particlePosns;
         tentacles.zeroDataTexture(ofxGpuParticles::VELOCITY);
@@ -384,6 +374,7 @@ namespace itg
             }
         }
         
+        /*
         ofSetColor(0, 255, 0);
         for (unsigned i = 0; i < jellies.size(); ++i)
         {
@@ -392,7 +383,7 @@ namespace itg
             {
                 ofCircle(deformed[j], 4.f);
             }
-        }
+        }*/
         
         // tentacles
         ofPushStyle();
