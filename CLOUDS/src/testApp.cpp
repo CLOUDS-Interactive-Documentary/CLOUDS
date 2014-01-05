8f0deaca820dfbb8768856f515ce44f914d69bb5
James George <james@jamesgeorge.org>
2014-01-04 21:02:28 -0500

    removed audio requested

--- CLOUDS/src/testApp.cpp
+++ CLOUDS/src/testApp.cpp
@@ -115,10 +115,10 @@ void testApp::keyPressed(int key){
     
 }
 
-//--------------------------------------------------------------
-void testApp::audioRequested(float * output, int bufferSize, int nChannels) {
-	mixer.fillBuffer(output,bufferSize,nChannels);
-}
+////--------------------------------------------------------------
+//void testApp::audioRequested(float * output, int bufferSize, int nChannels) {
+//	mixer.fillBuffer(output,bufferSize,nChannels);
+//}
 
 //--------------------------------------------------------------
 void testApp::keyReleased(int key){
