5cd2edcb4ea0dd97fe8f0e6dbcfe7455f45ab617
James George <james@jamesgeorge.org>
2014-01-07 03:33:44 -0500

    code labels

--- CloudsLibrary/src/VisualSystems/Code/vs_src/CloudsVisualSystemCode.cpp
+++ CloudsLibrary/src/VisualSystems/Code/vs_src/CloudsVisualSystemCode.cpp
@@ -11,12 +11,14 @@ bool rectareasort(ofRectangle a, ofRectangle b){
 }
 
 bool rectsort(ofRectangle a, ofRectangle b){
-	return a.x > b.x;
+	return a.x < b.x;
 }
 
 //These methods let us add custom GUI parameters and respond to their events
 void CloudsVisualSystemCode::selfSetupGui(){
 
+	
+	////////////PANELS
 	panelsGui = new ofxUISuperCanvas("PANELS", gui);
 	panelsGui->copyCanvasStyle(gui);
 	panelsGui->copyCanvasProperties(gui);
@@ -30,15 +32,15 @@ void CloudsVisualSystemCode::selfSetupGui(){
 	ofAddListener(panelsGui->newGUIEvent, this, &CloudsVisualSystemCode::selfGuiEvent);
 	guis.push_back(panelsGui);
 	guimap[panelsGui->getName()] = panelsGui;
-
+	////////////PANELS
+	
+	////////////TYPE
 	typeGui = new ofxUISuperCanvas("TYPE", gui);
 	typeGui->copyCanvasStyle(gui);
 	typeGui->copyCanvasProperties(gui);
 	typeGui->setName("Type");
 	typeGui->setWidgetFontSize(OFX_UI_FONT_SMALL);
 
-
-	typeGui->addRangeSlider("SPEED RANGE", .01, 1, &speedRange.min, &speedRange.max);
 	typeGui->addIntSlider("FONT SIZE", 4, 12, &fontSize);
 	typeGui->addSlider("TRACKING", 0., 3.0, &typeTracking);
 	typeGui->addSlider("LEFT MARGIN", 0, 20, &typeMarginLeft);
@@ -46,26 +48,48 @@ void CloudsVisualSystemCode::selfSetupGui(){
 	ofAddListener(typeGui->newGUIEvent, this, &CloudsVisualSystemCode::selfGuiEvent);
 	guis.push_back(typeGui);
 	guimap[typeGui->getName()] = typeGui;
-
+	//////////END TYPE
 	
+	//////////BOXES
 	boxGui = new ofxUISuperCanvas("BOXES", gui);
 	boxGui->copyCanvasStyle(gui);
 	boxGui->copyCanvasProperties(gui);
-	
+	boxGui->setName("Boxes");
+	boxGui->setWidgetFontSize(OFX_UI_FONT_SMALL);
+
 	boxGui->addSlider("WIDTH", 40, 300, &maxwidth);
 	boxGui->addSlider("HEIGHT", 40, 500, &maxheight);
-	
 	//any width/height smaller thin this will become a widget
 	boxGui->addIntSlider("min subdivision size", 4, 200, &minsize);
 	boxGui->addSlider("min code window size", 40, 500, &minTextboxSize);
 	boxGui->addIntSlider("box gen seed", 1, 300, &boxSeed);
+	boxGui->addIntSlider("box gen seed 2", 1, 300, &boxSeed2);
 	boxGui->addSlider("outline alpha", 0, 1.0, &outlineAlpha);
 	
 	ofAddListener(boxGui->newGUIEvent, this, &CloudsVisualSystemCode::selfGuiEvent);
 	guis.push_back(boxGui);
 	guimap[boxGui->getName()] = boxGui;
+	//////////BOXES
+	   
+	//////////ANIMATION
+	animationGUi = new ofxUISuperCanvas("ANIMATION", gui);
+	animationGUi->copyCanvasStyle(gui);
+	animationGUi->copyCanvasProperties(gui);
+	animationGUi->setName("Animation");
+	animationGUi->setWidgetFontSize(OFX_UI_FONT_SMALL);
 	
+	animationGUi->addSlider("START TIME RANGE", 0, 4, &startTimeDelay);
+	animationGUi->addRangeSlider("EXPAND SPEED RANGE", 0, 2, &expandSpeed.min, &expandSpeed.max);
+	animationGUi->addRangeSlider("SPEED RANGE", .01, 1, &speedRange.min, &speedRange.max);
+	animationGUi->addSlider("10PRINT SPEED", 0, 50, &tenPrintSpeedBoost);
+	animationGUi->addToggle("CASCADE", &bCascadeMode);
 	
+	ofAddListener(animationGUi->newGUIEvent, this, &CloudsVisualSystemCode::selfGuiEvent);
+	guis.push_back(animationGUi);
+	guimap[animationGUi->getName()] = animationGUi;
+	//////////ANIMATION
+	
+	//////////COLOR
 	colorGui = new ofxUISuperCanvas("COLOR", gui);
 	colorGui->copyCanvasStyle(gui);
 	colorGui->copyCanvasProperties(gui);
@@ -95,6 +119,7 @@ void CloudsVisualSystemCode::selfSetupGui(){
 	ofAddListener(colorGui->newGUIEvent, this, &CloudsVisualSystemCode::selfGuiEvent);
 	guis.push_back(colorGui);
 	guimap[colorGui->getName()] = colorGui;
+	//////////COLOR
 }
 
 void CloudsVisualSystemCode::selfGuiEvent(ofxUIEventArgs &e){
@@ -110,19 +135,34 @@ void CloudsVisualSystemCode::selfGuiEvent(ofxUIEventArgs &e){
 }
 
 void CloudsVisualSystemCode::generatePanels(){
+
+	populatePanels(panelsFront, boxSeed);
+	populatePanels(panelsBack, boxSeed2);
+	
+	generatedPanelWidth = getCanvasWidth();
+	generatedPanelHeight = getCanvasHeight();
+	regeneratePanels = false;
+	
+	cout << "generating panels" << endl;
+}
+
+
+//Use system gui for global or logical settings, for exmpl
+void CloudsVisualSystemCode::populatePanels(vector<Panel*>& panels, int seed){
+	
 	for(int i = 0; i < panels.size(); i++){
 		delete panels[i];
 	}
-	
 	panels.clear();
-	rectTests.clear();
 	
 	ofxMtlBoxFitting boxFitting;
 	boxFitting.setup(getCanvasWidth(),getCanvasHeight(),
 					 maxwidth, maxheight, minsize);
-
-	boxFitting.generate(boxSeed);
 	
+	boxFitting.generate(seed);
+	
+	vector<ofRectangle> rectTests;
+	ofRectangle allRects;
 	for(int i = 0; i < boxFitting.boxes.size(); i++) {
 		ofRectangle r = ofRectangle(boxFitting.boxes[i].x,
 									boxFitting.boxes[i].y,
@@ -133,10 +173,18 @@ void CloudsVisualSystemCode::generatePanels(){
 		r.width  -= 2;
 		r.height -= 2;
 		
+		allRects.growToInclude(r);
+		
 		rectTests.push_back(r);
 	}
 	
-	sort(rectTests.begin(),rectTests.end(), rectsort);
+	ofVec2f topOffset = ofVec2f((getCanvasWidth() - allRects.width) / 2.0,
+								(getCanvasHeight() - allRects.height) / 2.0);
+	for(int i = 0; i < rectTests.size(); i++){
+		rectTests[i].translate(topOffset);
+	}
+	sort(rectTests.begin(), rectTests.end(), rectsort);
+	
 	vector<ofRectangle> rectCopies = rectTests;
 	sort(rectCopies.begin(), rectCopies.end(), rectareasort);
 	
@@ -149,19 +197,30 @@ void CloudsVisualSystemCode::generatePanels(){
 		}
 		else{
 			if(bEnableConsole && bEnableTenPrint){
-				p = (ofRandomuf() > .5) ? (Panel*)new PanelConsole() : (Panel*)new PanelTenPrint();
+				if(ofRandomuf() > .5){
+					p = new PanelConsole();
+				}
+				else {
+					p = new PanelTenPrint();
+					p->speedBoost = &tenPrintSpeedBoost;					
+				}
 			}
 			else if(bEnableConsole){
 				p = new PanelConsole();
 			}
 			else{
 				p = new PanelTenPrint();
+				p->speedBoost = &tenPrintSpeedBoost;
 			}
 		}
-
+		
+		p->bgColor = &bgColor;
 		p->dataPath = getVisualSystemDataPath();
 		p->sharedFont = &sharedFont;
 		p->sharedLayout = &sharedLayout;
+		
+		p->startTimeDelay = &startTimeDelay;
+		p->expandSpeed = &expandSpeed;
 
 		p->outlineAlpha = &outlineAlpha;
 		p->marginLeft = &typeMarginLeft;
@@ -169,15 +228,10 @@ void CloudsVisualSystemCode::generatePanels(){
 		p->scanSpeed = powf(ofRandom(speedRange.min,speedRange.max),2);
 		p->drawRect = rectTests[i];
 		p->setup();
-
+		
 		panels.push_back(p);
 	}
 	
-	generatedPanelWidth = getCanvasWidth();
-	generatedPanelHeight = getCanvasHeight();
-	regeneratePanels = false;
-	
-	cout << "generating panels" << endl;
 }
 
 //Use system gui for global or logical settings, for exmpl
@@ -198,6 +252,7 @@ void CloudsVisualSystemCode::guiRenderEvent(ofxUIEventArgs &e){
 }
 
 void CloudsVisualSystemCode::selfSetDefaults(){
+	
 	regeneratePanels = false;
 	fontSize = 5;
 	currentFontSize = -1;
@@ -207,8 +262,22 @@ void CloudsVisualSystemCode::selfSetDefaults(){
 	bEnableCode = true;
 	bEnableConsole = true;
 	bEnableTenPrint = true;
+
+
+	boxSeed  =   0;
+	boxSeed2 = 100;
 	
 	typeMarginLeft = 5;
+	
+	tenPrintSpeedBoost = 20;
+	
+	startTimeDelay  =  .3;
+	expandSpeed.min =  .5;
+	expandSpeed.max = 1.0;
+	speedRange.min  =  .4;
+	speedRange.max  =  .9;
+	
+	bCascadeMode = false;
 	
 }
 // selfSetup is called when the visual system is first instantiated
@@ -230,8 +299,8 @@ void CloudsVisualSystemCode::selfPresetLoaded(string presetPath){
 // this is a good time to prepare for transitions
 // but try to keep it light weight as to not cause stuttering
 void CloudsVisualSystemCode::selfBegin(){
-	for(int i = 0; i < panels.size(); i++){
-		panels[i]->startAnimation();
+	for(int i = 0; i < panelsFront.size(); i++){
+		panelsFront[i]->startAnimation();
 	}
 }
 
@@ -256,6 +325,7 @@ void CloudsVisualSystemCode::updateColors(){
 
 //normal update call
 void CloudsVisualSystemCode::selfUpdate(){
+	
 	if(currentFontSize != fontSize){
 		sharedFont.loadFont(GetCloudsDataPath() + "font/Consolas.ttf", fontSize);
 		sharedLayout.loadFont(GetCloudsDataPath() + "font/Consolas.ttf", fontSize);
@@ -269,17 +339,34 @@ void CloudsVisualSystemCode::selfUpdate(){
 	   getCanvasHeight() != generatedPanelHeight )
 	{
 		generatePanels();
-		for(int i = 0; i < panels.size(); i++){
-			panels[i]->startAnimation();
+		for(int i = 0; i < panelsFront.size(); i++){
+			panelsFront[i]->startAnimation();
 		}
 	}
 	
 	updateColors();
+	for(int i = 0; i < panelsBack.size(); i++){
+		panelsBack[i]->tint = tintColor;
+		panelsBack[i]->matchColorTypes = matchColorTypes;
+		panelsBack[i]->update();
+	}
 	
-	for(int i = 0; i < panels.size(); i++){
-		panels[i]->tint = tintColor;
-		panels[i]->matchColorTypes = matchColorTypes;
-		panels[i]->update();
+	bool allFinished = true;
+	for(int i = 0; i < panelsFront.size(); i++){
+		panelsFront[i]->tint = tintColor;
+		panelsFront[i]->matchColorTypes = matchColorTypes;
+		panelsFront[i]->update();
+		allFinished &= panelsFront[i]->isDone();
+	}
+	
+	if(bCascadeMode && allFinished){
+		for(int i = 0; i < panelsBack.size(); i++){
+			panelsBack[i]->resetAnimation();
+		}
+		swap(panelsBack, panelsFront);
+		for(int i = 0; i < panelsFront.size(); i++){
+			panelsFront[i]->startAnimation();
+		}
 	}
 }
 
@@ -297,8 +384,14 @@ void CloudsVisualSystemCode::selfDrawDebug(){
 // or you can use selfDrawBackground to do 2D drawings that don't use the 3D camera
 void CloudsVisualSystemCode::selfDrawBackground(){
 
-	for(int i = 0; i < panels.size(); i++){
-		panels[i]->draw( getCanvasHeight() );
+	if(bCascadeMode){
+		for(int i = 0; i < panelsBack.size(); i++){
+			panelsBack[i]->draw( getCanvasHeight() );
+		}
+	}
+	
+	for(int i = 0; i < panelsFront.size(); i++){
+		panelsFront[i]->draw( getCanvasHeight() );
 	}
 }
 
