d55daa00870f48e89fcad0fa4128a094c80e1c3e
Gal Sasson <sasgal@gmail.com>
2014-01-07 00:35:20 -0500

    added VSRandomDigits2

---
+++ CloudsLibrary/src/VisualSystems/RandomDigits2/vs_src/Rand.h
@@ -0,0 +1,72 @@
+//
+//  Rand.h
+//  OpenP5SpinningSolids
+//
+//  Created by Jonathan Minard on 11/2/13.
+//
+//
+
+#pragma once 
+#include "ofMain.h"
+#include "ofxTween.h"
+
+class Rand {
+    
+public:
+    
+    int ID; 
+    float posX;
+    float posY;
+    int randomNumber;
+    
+ 
+    int* minBri;
+    int* maxBri;
+    
+    Rand(float posX, float posY, float posZ,
+         int& minBri, int& maxBri);
+    void setup(); 
+    void noiseRotate(float _x, float _y);
+    void generateNoisyNumber();
+    void generateRandomNumber();
+    void changeRandomNumber();
+    void counter();
+    void increaseRandomNumber();
+    void drawNumbers();
+    void update();
+
+    int index =0;
+    float previousRandomNumber;
+    float nextRandomNumber;
+    float currentTime;
+    float previousTime;
+    // text
+    //FTGL
+    //string text;
+    //float fontSize;
+    //static ofxFTGLSimpleLayout font;
+    
+    string typeStr;
+    
+    ofTrueTypeFont  franklinBook14;
+    ofTrueTypeFont	verdana14;
+    static ofTrueTypeFont  Font;
+    
+protected:
+    
+    float rx;
+    float ry;
+    float speed = .02;
+    float rotNoise; 
+    
+    float period = 120; //period of oscillation
+    
+    float previousMouseX = 0;
+    float previousMouseY = 0;
+    
+    int tweenRandom;
+    
+    int i = 0;
+
+    
+};
---
+++ VSRandomDigits2/src/testApp.cpp
@@ -0,0 +1,75 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+	vs.setup();
+    vs.loadPresetGUISFromName("Working");
+	vs.playSystem();
+    
+    mixer.setup();
+    mixer.setDiageticVolume(1);
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::exit(){
+    vs.exit();
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
+
+void testApp::audioRequested(float *output, int bufferSize, int nChannels)
+{
+    mixer.fillBuffer(output, bufferSize, nChannels);
+}
