3771b7dc4f589dd3405c52fbe9b29703cb4fdd22
jonathanminard <jonathanminard@gmail.com>
2014-01-13 02:53:43 -0500

    Presets

--- CloudsLibrary/src/VisualSystemsLibrary/CloudsVisualSystem.h
+++ CloudsLibrary/src/VisualSystemsLibrary/CloudsVisualSystem.h
@@ -7,7 +7,7 @@
 #include "ofxTLCameraTrack.h"
 #include "CloudsRGBDCamera.h"
 #include "CloudsInputEvents.h"
-#include "CloudsHUDController.h"
+//#include "CloudsHUDController.h"
 #include "ofxLight.h"
 #include "ofxGenerative.h"
 //#include "ofxMaterial.h"
