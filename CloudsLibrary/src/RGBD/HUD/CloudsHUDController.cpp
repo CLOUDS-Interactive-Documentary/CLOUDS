3846b02692b1999b962b462b5a1978eaa48a4653
Elie Zananiri <prisonerjohn@gmail.com>
2014-01-16 16:26:48 -0700

    HUD: Only drawing home if lower third is on screen.

--- CloudsLibrary/src/RGBD/HUD/CloudsHUDController.cpp
+++ CloudsLibrary/src/RGBD/HUD/CloudsHUDController.cpp
@@ -568,9 +568,9 @@ void CloudsHUDController::draw(){
         (it->second)->draw();
     }
     
-	if(bDrawHome)
+	if (bDrawHome && hudOpenMap[CLOUDS_HUD_LOWER_THIRD]){
 		home.draw();
-	
+    }
 	
 	ofPopMatrix();
 	ofPopStyle();
@@ -683,7 +683,7 @@ void CloudsHUDController::drawLayer3D(CloudsHUDLayerSet layer, ofCamera* cam, of
     drawLayer(layer);
     
     // Draw the home button if we're on the right layer.
-    if (layer == CLOUDS_HUD_LOWER_THIRD && bDrawHome) {
+    if (layer == CLOUDS_HUD_LOWER_THIRD && bDrawHome && hudOpenMap[CLOUDS_HUD_LOWER_THIRD]) {
         home.draw();
     }
     
