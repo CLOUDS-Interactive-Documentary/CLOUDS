0a339c302472a1652d49aa65845b1f7b789e6a6c
James George <james@jamesgeorge.org>
2014-03-06 15:20:22 -0600

    index saving and rift only for playlist

--- CloudsVisualSystemPlaylist/src/testView.mm
+++ CloudsVisualSystemPlaylist/src/testView.mm
@@ -11,22 +11,52 @@
 	
 	visualSystems.loadPresets();
     visualSystems.loadCachedDataForSystems();
-		
+	for(int i = 0; i < visualSystems.getPresets().size(); i++){
+		if(visualSystems.getPresets()[i].enabledOculus){
+			riftIndeces.push_back(i);
+		}
+	}
+	
     [presetTable setTarget:self];
 	[presetTable setDoubleAction:@selector(playPlaylist:)];
 	[presetTable reloadData];
 
-	maxSystemDuration = 35.0;
+	maxSystemDuration = 50.0;
 	
     mixer.setup();
     mixer.setDiageticVolume(1);
-
+	
+	string path = "index_save.txt";
+	if(ofFile(path).exists()){
+		ofBuffer b = ofBufferFromFile(path);
+		NSMutableIndexSet* set = [NSMutableIndexSet indexSet];
+		
+		vector<string> components = ofSplitString(b.getText(), ",");
+		for(int i = 0; i < components.size(); i++){
+			[set addIndex:ofToInt(components[i])];
+		}
+		[presetTable selectRowIndexes:set byExtendingSelection:NO];
+	}
 }
 
 
 - (IBAction) playPlaylist:(id)sender
 {
 	shouldPlayPlaylist = true;
+	
+	//save a file of indices
+	vector<string> indeces;
+
+	NSUInteger idx = [presetTable.selectedRowIndexes firstIndex];
+	while (idx != NSNotFound) {
+		indeces.push_back( ofToString(idx) );
+		idx = [presetTable.selectedRowIndexes indexGreaterThanIndex:idx];
+	}
+	
+	ofBuffer buf(ofJoinString(indeces, ","));
+	string path = "index_save.txt";
+	ofBufferToFile(path, buf);
+
 }
 
 - (void)update
@@ -34,6 +64,7 @@
 
 	if(shouldPlayPlaylist){
 		
+		isPlayingPlaylist = false;
 		shouldPlayPlaylist = false;
 		playlist.clear();
 		currentVisualSystem = NULL;
@@ -44,7 +75,7 @@
 			NSUInteger idx = [presetTable.selectedRowIndexes firstIndex];
 			while (idx != NSNotFound) {
 			
-				CloudsVisualSystemPreset& preset = visualSystems.getPresets()[ idx ];
+				CloudsVisualSystemPreset& preset = visualSystems.getPresets()[ riftIndeces[idx] ];
 				CloudsVisualSystem* system = CloudsVisualSystemManager::InstantiateSystem( preset.systemName ) ;
 				if(system == NULL){
 					continue;
@@ -73,114 +104,6 @@
 		}
 	}
 	
-	/*
-	if(runningTest){
-		
-		if(ofGetElapsedTimef() - lastSystemStartTime > 1){
-			
-			if(currentVisualSystem != NULL){
-				cout << "5) (" << currentTestPresetIndex << "/" << testPresetIndeces.size() << ") STOPPING SYSTEM " << currentVisualSystem->getSystemName() << endl;
-				currentVisualSystem->stopSystem();
-			}
-
-			if(testBatchIndex == testBatch.size() || testBatch.size() == 0){
-				
-				for(int i = 0; i < testBatch.size(); i++){
-					cout << "5) CALLING EXIT " << testBatch[i]->getSystemName() << endl;
-					testBatch[i]->exit();
-				}
-				
-				cout << "DELETING PRESET BATCH" << endl;
-				
-                testBatch.clear();
-
-
-                currentVisualSystem = NULL;
-				
-				int i = 0;
-				for(i = currentTestPresetIndex; i < MIN(testPresetIndeces.size(),currentTestPresetIndex+2); i++){
-					
-					CloudsVisualSystemPreset& preset = visualSystems.getPresets()[ testPresetIndeces[i] ];
-					cout << "******* 1) (" << i << "/" << testPresetIndeces.size() << ") INSTANTIATING " << preset.systemName << " : " << preset.presetName << endl;
-					CloudsVisualSystem* vs = CloudsVisualSystemManager::InstantiateSystem( preset.systemName );
-					cout << "******* 2) (" << i << "/" << testPresetIndeces.size() << ") SETTING UP " << preset.systemName << " : " << preset.presetName << endl;
-					if(vs == NULL){
-						cout << "******* 3) (" << i << "/" << testPresetIndeces.size() << ") PRESET NOT REGISTERED " << preset.systemName << " : " << preset.presetName << endl;
-						continue;
-					}
-					vs->setup();
-					cout << "******* 3) (" << i << "/" << testPresetIndeces.size() << ") LOADING PRESET " << preset.systemName << " : " << preset.presetName << endl;
-					vs->loadPresetGUISFromName(preset.presetName);
-					
-					testBatch.push_back(vs);
-				}
-				
-				if(i == testPresetIndeces.size()){
-					runningTest = false;
-				}
-				
-				testBatchIndex = 0;
-			}
-			
-			
-			if(testBatchIndex < testBatch.size()){
-				currentVisualSystem = testBatch[testBatchIndex];
-				cout << "4) PLAYING SYSTEM " << currentVisualSystem->getSystemName() << endl;
-				currentVisualSystem->playSystem();
-				lastSystemStartTime = ofGetElapsedTimef();
-				
-				testBatchIndex++;
-				currentTestPresetIndex++;
-                if(currentTestPresetIndex == testPresetIndeces.size()-1){
-                    cout<<"TEST COMPLETE : Reshufling and looping test"<<endl;
-                    currentTestPresetIndex = 0;
-                    random_shuffle(testPresetIndeces.begin(), testPresetIndeces.end());
-                }
-			}
-			
-		}
-	}
-    else if(shouldPlaySelectedRow){
-        
-		if(currentVisualSystem != NULL){
-            currentVisualSystem->stopSystem();
-			currentVisualSystem->exit();
-            currentVisualSystem = NULL;
-        }
-
-		cout << "loading system " << visualSystems.getPresets()[ self.selectedPresetIndex ].systemName << " preset " << visualSystems.getPresets()[self.selectedPresetIndex].presetName << endl;
-        
-        visualSystems.DeallocateSystems();
-        currentVisualSystem = CloudsVisualSystemManager::InstantiateSystem( visualSystems.getPresets()[ self.selectedPresetIndex ].systemName );
-		
-		///SCREENCAPTURE MODE
-		currentVisualSystem->setNumSamples(4);
-		currentVisualSystem->forceScreenResolution(1920, 1080);
-		currentVisualSystem->setDrawToScreen(false);
-		/////
-		
-		if(currentVisualSystem != NULL){
-			currentVisualSystem->setup();
-			string presetName = visualSystems.getPresets()[ self.selectedPresetIndex ].presetName;
-			if(presetName == "+New Preset"){
-				currentVisualSystem->loadPresetGUISFromName("");
-			}
-			else if(presetName == "+Current State"){
-				currentVisualSystem->loadPresetGUISFromName("Working");
-			}
-			else {
-				currentVisualSystem->loadPresetGUISFromName(presetName);
-			}
-			currentVisualSystem->playSystem();
-		}
-		else{
-			ofSystemAlertDialog(visualSystems.getPresets()[self.selectedPresetIndex].systemName + " is not registered system");
-		}
-		
-		shouldPlaySelectedRow = false;	
-    }
-	 */
-	
 }
 
 - (void)draw
@@ -200,7 +123,7 @@
 	
 	playlistIndex++;
 	
-	if(playlistIndex == playlist.size()){
+	if(playlistIndex >= playlist.size()){
 		//restart the same playlist
 		shouldPlayPlaylist = true;
 	}
@@ -242,7 +165,9 @@
 
 - (void)mousePressed:(NSPoint)p button:(int)button
 {
-	[self gotoNextSystem];
+	if(isPlayingPlaylist){
+		[self gotoNextSystem];
+	}
 }
 
 - (void)mouseReleased:(NSPoint)p button:(int)button
@@ -257,17 +182,23 @@
 
 - (NSInteger)numberOfRowsInTableView:(NSTableView *)aTableView
 {
-	return visualSystems.getPresets().size();
+//	return visualSystems.getPresets().size();
+	return riftIndeces.size();
 }
 
 - (id)tableView:(NSTableView *)aTableView objectValueForTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex
 {
 	
-	int presetIndex = rowIndex;
+//	int presetIndex = rowIndex;
+	int presetIndex = riftIndeces[rowIndex];
 	
 	if([@"system" isEqualToString:aTableColumn.identifier]){
 		return [NSString stringWithUTF8String: visualSystems.getPresets()[presetIndex].systemName.c_str()];
 	}
+	else if([@"preset" isEqualToString:aTableColumn.identifier]){
+		return [NSString stringWithUTF8String: visualSystems.getPresets()[presetIndex].presetName.c_str()];
+	}
+	
 //	else if([@"grade" isEqualToString:aTableColumn.identifier]){
 //		return [NSString stringWithUTF8String:
 //				(string(visualSystems.getPresets()[presetIndex].hasFiles ? "" : "!!") +
@@ -277,9 +208,6 @@
 //						(visualSystems.getPresets()[presetIndex].hasSound() ? "Snd" : "")
 //				 ).c_str()];
 //	}
-	else if([@"preset" isEqualToString:aTableColumn.identifier]){
-		return [NSString stringWithUTF8String: visualSystems.getPresets()[presetIndex].presetName.c_str()];
-	}
 //	else if( [@"keywords" isEqualToString:aTableColumn.identifier] ){
 //		return [NSString stringWithUTF8String: ofJoinString( visualSystems.keywordsForPreset(presetIndex), ",").c_str() ];
 //	}
