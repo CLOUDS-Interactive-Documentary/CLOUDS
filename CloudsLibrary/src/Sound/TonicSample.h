d8205a4fe52ab19fd0cc5d2ee6b0273d14f2ea22
Render <render@Render1.local>
2014-04-20 01:55:56 -0400

    updates

--- CloudsLibrary/src/Sound/TonicSample.h
+++ CloudsLibrary/src/Sound/TonicSample.h
@@ -8,7 +8,23 @@
 
 #ifndef CloudsVisualSystemEditor_TonicSample_h
 #define CloudsVisualSystemEditor_TonicSample_h
+#include "ofxTonic.h"
 
+class TonicSample{
+public:
+    TonicSample(){
+//        soundFile = " ";
+        playSample = false;
+    }
+    TonicSample(string _soundFile){
+        soundFile = _soundFile;
+    
+    }
+    string soundFile;
+    bool playSample;
+    Tonic::ControlTrigger soundTrigger;
+    
+};
 
 
 #endif
