c13723743d95ec62f6867269f35ecb29fcdb3768
Render <render@Render1.local>
2014-04-20 01:55:46 -0400

    TonicSample in colony and connectors

---
+++ CloudsLibrary/src/Sound/TonicSample.h
@@ -0,0 +1,14 @@
+//
+//  TonicSample.h
+//  CloudsVisualSystemEditor
+//
+//  Created by Render on 4/20/14.
+//
+//
+
+#ifndef CloudsVisualSystemEditor_TonicSample_h
+#define CloudsVisualSystemEditor_TonicSample_h
+
+
+
+#endif
