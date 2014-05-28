1230e6fa0ed28ed7e9dcdbda3cbc977be441a407
Elie Zananiri <prisonerjohn@gmail.com>
2014-05-28 17:36:05 -0400

    Histogram: Set defaults.

--- CloudsLibrary/src/VisualSystems/Histogram/vs_src/CloudsVisualSystemHistogram.h
+++ CloudsLibrary/src/VisualSystems/Histogram/vs_src/CloudsVisualSystemHistogram.h
@@ -52,6 +52,7 @@ class CloudsVisualSystemHistogram : public CloudsVisualSystem {
 	// This will be called during a "loading" screen, so any big images or
 	// geometry should be loaded here
     void selfSetup();
+    void selfSetDefaults();
 
 	// selfBegin is called when the system is ready to be shown
 	// this is a good time to prepare for transitions
@@ -86,7 +87,6 @@ class CloudsVisualSystemHistogram : public CloudsVisualSystem {
 
 	// this is called when you should clear all the memory and delet anything you made in setup
     void selfExit();
-    void selfSetDefaults();
 
 	//events are called when the system is active
 	//Feel free to make things interactive for you, and for the user!
@@ -120,12 +120,10 @@ protected:
     vector<float> dataPoints;
     int maxNumDataPoints;
     
-    int seed;
-    int stepSize;
     int noiseValue;
+    int n;
     
     float t;
-    int n;
     
     ofDirectory soundsDir;
     int selectedSoundsIdx;
