012cf02cae7e72a7df03220be3abf32f061dc71b
Elie Zananiri <prisonerjohn@gmail.com>
2014-06-09 01:29:46 -0400

    Cleaned up.

--- CloudsLibrary/src/RGBD/CloudsCalibrationNode.cpp
+++ CloudsLibrary/src/RGBD/CloudsCalibrationNode.cpp
@@ -144,7 +144,7 @@ void CalibrationNode::draw(){
 		arc.setStrokeWidth(3);
 		arc.setStrokeColor(arcColor);
 		float animationProgress = ofxTween::map(percentComplete, 0.0, 1.0, 0.0, 1.0, true, ofxEasingQuad(), ofxTween::easeOut);
-        arc.moveTo(0, nodeSize, 0);  // EZ: This is necessary under Windows
+        arc.moveTo(0, nodeSize, 0);
 		arc.arc(ofVec3f(0,0,0), nodeSize, nodeSize, 90, 360*animationProgress + 90, true);
 		arc.draw();
 	}
