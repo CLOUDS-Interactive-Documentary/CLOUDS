2cfa6d08aa14957b22061f6fe0c051f69effae13
James George <james@jamesgeorge.org>
2014-06-09 00:49:19 -0400

    fixed draw overlay flip

--- CloudsLibrary/src/VisualSystemsLibrary/CloudsVisualSystem.cpp
+++ CloudsLibrary/src/VisualSystemsLibrary/CloudsVisualSystem.cpp
@@ -609,11 +609,7 @@ void CloudsVisualSystem::draw(ofEventArgs & args)
             
 			ofPushStyle();
 			ofPushMatrix();
-			ofTranslate(0, getCanvasHeight() );
-			ofScale(1,-1,1);
-			
 			selfDrawOverlay();
-
 			ofPopMatrix();
 			ofPopStyle();
 	
