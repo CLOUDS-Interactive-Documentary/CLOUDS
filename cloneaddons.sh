c2a1e547cd447425bf3839dce5567f7a46c5e190
Elie Zananiri <prisonerjohn@gmail.com>
2015-04-23 16:26:58 -0400

    Organized addons alphabetically for my sanity. Added ofxSSL and ofxAudioDecoder.

--- cloneaddons.sh
+++ cloneaddons.sh
@@ -20,27 +20,37 @@ else
 	echo "Cloning read-only"
 fi
 
-${NEWPREFIX}ofxUI
-cd ofxUI
-git checkout -b 'clouds-64' --track 'origin/clouds-64'
-git pull origin 'clouds-64'
-cd ..
-
-${NEWPREFIX}ofxCocoaGLView
-${NEWPREFIX}ofxPCL
-${NEWPREFIX}ofxMSAObjCPointer
-${NEWPREFIX}ofxMSACore
-${NEWPREFIX}ofxCameraSaveLoad
-${NEWPREFIX}ofxMSAPhysics
-${NEWPREFIX}ofxMarchingCubes
-${NEWPREFIX}ofxAutoReloadedShader
-${NEWPREFIX}ofxSQLite
-${NEWPREFIX}ofxTextWriter
-${NEWPREFIX}ofxBillboard
-${NEWPREFIX}ofxRegex
 ${NEWPREFIX}ofxAnimationPrimitives
+${NEWPREFIX}ofxAudioDecoder
+${NEWPREFIX}ofxAudioFeatures
+${NEWPREFIX}ofxAudioUnit
+${NEWPREFIX}ofxAutoReloadedShader
+${NEWPREFIX}ofxAVFVideoPlayer
+${NEWPREFIX}ofxBillboard
+${NEWPREFIX}ofxBinaryMesh
 ${NEWPREFIX}ofxBox2d
+${NEWPREFIX}ofxCameraSaveLoad
+${NEWPREFIX}ofxCocoaGLView
+${NEWPREFIX}ofxColorPalettes
+${NEWPREFIX}ofxCv
+${NEWPREFIX}ofxDelaunay
+${NEWPREFIX}ofxFTGL
+${NEWPREFIX}ofxGameCamera
+${NEWPREFIX}ofxGenerative
+${NEWPREFIX}ofxGpuParticles
 ${NEWPREFIX}ofxLocalization
+${NEWPREFIX}ofxMarchingCubes
+${NEWPREFIX}ofxMeshUtils
+${NEWPREFIX}ofxMPMFluid
+${NEWPREFIX}ofxMSACore
+${NEWPREFIX}ofxMSAInteractiveObject
+${NEWPREFIX}ofxMSAObjCPointer
+${NEWPREFIX}ofxMSAPhysics
+${NEWPREFIX}ofxMSATimer
+${NEWPREFIX}ofxNearestNeighbour
+${NEWPREFIX}ofxObjLoader	
+${NEWPREFIX}ofxOcean
+${NEWPREFIX}ofxOculusRift
 
 ${NEWPREFIX}ofxOpenSteer
 cd ofxOpenSteer
@@ -48,15 +58,7 @@ git checkout -b feature-64bit --track origin/feature-64bit
 git pull origin feature-64bit
 cd ..
 
-${NEWPREFIX}ofxSimpleSpline
-${NEWPREFIX}ofxSimpleSurface
-${NEWPREFIX}ofxTonic
-${NEWPREFIX}ofxPtf
-${NEWPREFIX}ofxNearestNeighbour
-${NEWPREFIX}ofxSubtitles
-${NEWPREFIX}ofxBinaryMesh
-${NEWPREFIX}ofxCv
-${NEWPREFIX}ofxGpuParticles
+${NEWPREFIX}ofxPCL
 
 ${NEWPREFIX}ofxPostProcessing
 cd ofxPostProcessing
@@ -64,28 +66,9 @@ git checkout -b develop --track origin/develop
 git pull origin develop
 cd ..
 
-${NEWPREFIX}ofxGenerative
-${NEWPREFIX}ofxAVFVideoPlayer
-${NEWPREFIX}ofxOculusRift
-${NEWPREFIX}ofxColorPalettes
-${NEWPREFIX}ofxOcean
-${NEWPREFIX}ofxGameCamera
+${NEWPREFIX}ofxPtf
 ${NEWPREFIX}ofxRange
-${NEWPREFIX}ofxTween
-${NEWPREFIX}ofxMSAInteractiveObject
-${NEWPREFIX}ofxFTGL
-${NEWPREFIX}ofxMPMFluid
-${NEWPREFIX}ofxDelaunay
-
-${NEWPREFIX}ofxAudioUnit
-${NEWPREFIX}ofxAudioFeatures
-${NEWPREFIX}ofxMeshUtils
-
-${NEWPREFIX}ofxTextInputField
-cd ofxTextInputField
-git checkout -b develop --track origin/develop
-git pull origin develop
-cd ..
+${NEWPREFIX}ofxRegex
 
 ${NEWPREFIX}ofxRGBDepth
 cd ofxRGBDepth
@@ -93,13 +76,32 @@ git checkout -b develop --track origin/develop
 git pull origin develop
 cd ..
 
-${NEWPREFIX}ofxMSATimer
-${NEWPREFIX}ofxTimecode
-${NEWPREFIX}ofxTimeline
+${NEWPREFIX}ofxSimpleSpline
+${NEWPREFIX}ofxSimpleSurface
+${NEWPREFIX}ofxSQLite
+${NEWPREFIX}ofxSSL
+${NEWPREFIX}ofxSubtitles
 
+${NEWPREFIX}ofxTextInputField
+cd ofxTextInputField
+git checkout -b develop --track origin/develop
+git pull origin develop
+cd ..
+
+${NEWPREFIX}ofxTextWriter
+${NEWPREFIX}ofxTimecode
+
+${NEWPREFIX}ofxTimeline
 cd ofxTimeline
 git checkout -b develop --track origin/develop
 git pull origin develop
 cd ..
 
-${NEWPREFIX}ofxObjLoader	
+${NEWPREFIX}ofxTonic
+${NEWPREFIX}ofxTween
+
+${NEWPREFIX}ofxUI
+cd ofxUI
+git checkout -b 'clouds-64' --track 'origin/clouds-64'
+git pull origin 'clouds-64'
+cd ..
