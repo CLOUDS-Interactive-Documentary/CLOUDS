140252de17ff4556ba238dcfa165e79c9ed976a0
James George <james@jamesgeorge.org>
2015-05-30 19:10:26 -0400

    git ignore set up for new build process

---
+++ _GOLD/Builds/Projects/Mac/VHX/copy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+homedir=~
+eval homedir=$homedir
+echo $homedir # prints home path
+
+if ! [ -d "$homedir/Library/Application Support/CLOUDS" ]; then 
+	mkdir $homedir/Library/Application\ Support/CLOUDS;
+fi
+
+if [ -d "$homedir/Library/Application Support/CLOUDS" ]; then
+	mv $DSTROOT/CloudsData $homedir/Library/Application\ Support/CLOUDS;
+	rm -rf $DSTROOT/CloudsData;
+fi
+
+#mv $DSTROOT/dataRoot.txt ~/Library/Application\ Support/CLOUDS;
+
+open -a /Applications/CLOUDS.app
+
+exit 0
