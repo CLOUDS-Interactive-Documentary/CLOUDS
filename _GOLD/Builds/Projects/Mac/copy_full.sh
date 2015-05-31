3f3154b3ed093a037dbb804d4e2a38d432fba371
James George <james@jamesgeorg>
2015-05-30 23:16:32 -0400

    tweaking full copy script

--- _GOLD/Builds/Projects/Mac/copy_full.sh
+++ _GOLD/Builds/Projects/Mac/copy_full.sh
@@ -6,13 +6,13 @@ echo $homedir # prints home path
 mkdir $homedir/Library/Application\ Support/CLOUDS;
 
 if [ -d $homedir/Library/Application\ Support/CLOUDS ]; then
-	mv $DSTROOT/CloudsData $homedir/Library/Application\ Support/CLOUDS;
+	mv $DSTROOT/CloudsData $homedir/Library/Application\ Support/CLOUDS/;
 	rm -rf $DSTROOT/CloudsData;
 fi
 
-if [ -d /Volume/CLOUDS/.CloudsMedia.noindex ]; then
-mkdir $homedir/Library/Application\ Support/CLOUDS/.CloudsMedia.noindex;
-cp -r /Volume/CLOUDS/.CloudsMedia.noindex $homedir/Library/Application\ Support/CLOUDS/.CloudsMedia.noindex;
+if [ -d /Volumes/CLOUDS/.CloudsMedia.noindex ]; then
+
+sudo cp -rf /Volumes/CLOUDS/.CloudsMedia.noindex $homedir/Library/Application\ Support/CLOUDS/;
 fi
 
 mv $DSTROOT/mediaRoot.txt $homedir/Library/Application\ Support/CLOUDS;
