ef3a5c9bcf946de3858d10620edceb1f0aa7115f
James George <james@jamesgeorg>
2016-07-09 15:44:14 -0400

    clouds release check in

--- _GOLD/Builds/Projects/Mac/remove.sh
+++ _GOLD/Builds/Projects/Mac/remove.sh
@@ -3,12 +3,12 @@ if [ -d ~/Library/Application\ Support/CLOUDS/ ]; then
     sudo rm -rf ~/Library/Application\ Support/CLOUDS;
 fi
 
-if [ -d /Application/CLOUDS.app ]; then
-    sudo rm -rf /Application/CLOUDS.app;
+if [ -d /Applications/CLOUDS.app ]; then
+    sudo rm -rf /Applications/CLOUDS.app;
 fi
 
-if [ -d /Application/CLOUDS_OCULUS.app ]; then
-    sudo rm -rf /Application/CLOUDS_OCULUS.app;
+if [ -d /Applications/CLOUDS_OCULUS.app ]; then
+    sudo rm -rf /Applications/CLOUDS_OCULUS.app;
 fi
 
 exit 0
